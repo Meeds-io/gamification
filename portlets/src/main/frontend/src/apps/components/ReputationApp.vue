9c12249f583790a6eb212c3c9e17cabe3a2482e7
AmineChalouati <45559512+AmineChalouati@users.noreply.github.com>
2019-02-04 17:48:39 +0100

    Update ReputationApp.vue

--- portlets/src/main/frontend/src/apps/components/ReputationApp.vue
+++ portlets/src/main/frontend/src/apps/components/ReputationApp.vue
@@ -30,7 +30,8 @@
                     setTimeout(this.initMenuApp, 500);
                     return;
                 }
-                $('.userNavigation').append(` \  <li id='myGamificationTab' class='item${this.isMaximized ? ' active' : ''}'> \
+                $('.userNavigation').append(` \  
+                <li id='myGamificationTab' class='item${this.isMaximized ? ' active' : ''}'> \
             <a href='${eXo.env.portal.context}/${eXo.env.portal.portalName}/achievements'>
               <div class='uiIconAppGamification uiIconDefaultApp' /> \
               <span class='tabName'>My Achievements</span> \
