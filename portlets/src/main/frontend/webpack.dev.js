96f6c1bdb2351db2fbb6eb8eefc12c8440f0dd5a
AmineChalouati <45559512+AmineChalouati@users.noreply.github.com>
2019-02-11 17:09:28 +0100

    update webpack

--- portlets/src/main/frontend/webpack.dev.js
+++ portlets/src/main/frontend/webpack.dev.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const exoServerPath = "/home/exo/Documents/WorkingSpace/server/company-community";
 
 module.exports = merge(common, {
 
@@ -13,14 +12,12 @@ module.exports = merge(common, {
            badge: "./badge.js",
            reputation: "./reputation.js",
            leaderboard: "./leaderboard.js",
-           GamificationInformations: "./GamificationInformations.js",
            spaceleaderboard: "./spaceleaderboard.js",
 
        },
        output: {
-           path: path.resolve(__dirname, exoServerPath + '/webapps/gamification-portlets/javascript/'),
 
-           //path: path.resolve(__dirname, '../webapp/javascript/'),
+           path: path.resolve(__dirname, '../webapp/javascript/'),
            filename: '[name]/[name].bundle.js'
 
        },
