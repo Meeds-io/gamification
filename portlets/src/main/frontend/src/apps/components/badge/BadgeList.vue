b497c36f1d50adb9e8b6bdca28c3f69dbad55ed2
wafakd <wkdous@exoplatform.com>
2019-08-01 11:06:32 +0100

    fix edit mode for enabled badges

--- portlets/src/main/frontend/src/apps/components/badge/BadgeList.vue
+++ portlets/src/main/frontend/src/apps/components/badge/BadgeList.vue
@@ -6,35 +6,87 @@
                 <table class=" uiGrid table table-hover badge-table">
                     <thead>
                     <tr>
+
                         <th class="badge-title-col">Title</th>
                         <th class="badge-desc-col">Description</th>
                         <th class="badge-nedded-score-col">Needed Score</th>
                         <th class="badge-domain-col">Domain</th>
                         <th class="badge-icon-col">Icon</th>
                         <th class="badge-status-col">Status</th>
-
+                        <!--    <th class="badge-created-by-col">Created by</th> -->
                         <th class="badge-action-col">Action</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr v-for="badge in badges" track-by="id" v-on:click.prevent="onEdit(badge)">
-                        <td class="badge-title-col">{{badge.title}}</td>
-                        <td class="badge-desc-col">{{badge.description}}</td>
-                        <td class="badge-needed-score-col">
-                            <div v-if="badge.neededScore >=1000">{{badge.neededScore/1000}} K</div>
-                            <div v-if="badge.neededScore <1000"> {{badge.neededScore}}</div>
-
+                    <tr v-for="badge in badges" track-by="id">
+                        <td class="badge-title-col">
+                            <div v-if="editedbadge.id !== badge.id">{{badge.title}}</div>
+                            <input type="text" v-if="editedbadge.id === badge.id" v-model="badge.title"style="width: 130px;">
                         </td>
+                        <td class="badge-desc-col">
+                            <div v-if="editedbadge.id !== badge.id">{{badge.description}}</div>
+                            <input class="badge-desc-col" type="text" v-if="editedbadge.id === badge.id" v-model="badge.description" style="min-width: 98%;">
+                        </td>
+                        <td class="badge-needed-score-col">
+                            <div v-if="editedbadge.id !== badge.id">{{badge.neededScore}}</div>
+                            <input  class="badge-needed-score-col" type="text" v-if="editedbadge.id === badge.id" v-model="badge.neededScore">
+                        </td>
+                        <td class="badge-domain-col" style="max-width: 165px;">
+                            <div v-if="editedbadge.id !== badge.id">{{badge.domain}}</div>
 
-                        <td class="badge-domain-col">{{badge.domain}}</td>
-                        <td class="badge-icon-col"><img thumbnail fluid :src="`/rest/gamification/reputation/badge/${badge.title}/avatar`" alt="Thumbnail" class="m-1"  width="40" height="40"/></td>
-                        <td class="badge-status-col">{{badge.enabled}}</td>
+                            <select type="text" v-if="editedbadge.id === badge.id" v-model="badge.domain" class="mb-4" style="height: 38px;" required>
+                                <template slot="first">
 
+                                    <option :value="null" disabled></option>
+                                </template>
+
+                                <option value="Social">Social</option>
+                                <option value="Knowledge">Knowledge</option>
+                                <option value="Teamwork">Teamwork</option>
+                                <option value="Feedback">Feedback</option>
+
+                                <!--<option value="Content">Content</option>-->
+                            </select>
+                        </td>
+                        <td id="iconInputGroup">
+                            <div v-if="editedbadge.id !== badge.id"> <img thumbnail fluid :src="`/rest/gamification/reputation/badge/${badge.title}/avatar`" alt="Thumbnail" class="m-1"  width="40" height="40"/>
+                            </div>
+                                <div v-if="editedbadge.id === badge.id"> <b-form-file v-model="badge.icon" placeholder="Choose a file..." accept="image/jpeg, image/png, image/gif"></b-form-file>
+                            </div>
+                        </td>
+                        <td class="badge-status-col">
+                            <div v-if="editedbadge.id !== badge.id">
+                                <label class="switch" >
+                                    <input type="checkbox" v-model="badge.enabled">
+                                    <span class="slider round"></span>
+                                    <span class="absolute-no">NO</span>
+                                </label>
+                            </div>
+                            <div v-if="editedbadge.id === badge.id">
+                                <label class="switch" v-on:click ="badge.enabled = !badge.enabled">
+                                    <input type="checkbox" >
+                                    <span class="slider round"></span>
+                                    <span class="absolute-no">NO</span>
+                                </label>
+                            </div>
+                        </td>
+                        <!--  <td class="badge-created-date-col">
+                              <span v-if="editedbadge.id !== badge.id">{{badge.createdBy}}</span>
+                              <input type="text" v-if="editedbadge.id === badge.id" v-model="badge.createdBy" style="width: 55px;"></td> -->
                         <td class="center actionContainer">
-                            <a href="#" v-on:click.prevent.stop="onRemove(badge.id,badge.title)" data-placement="bottom" rel="tooltip" class="actionIcon"
+                            <a href="#" v-if="editedbadge.id !== badge.id" v-on:click.prevent.stop="onRemove(badge.id,badge.title)" data-placement="bottom" rel="tooltip" class="actionIcon"
                                data-original-title="Supprimer" v-b-tooltip.hover title="Supprimer">
                                 <i class="uiIconDelete uiIconLightGray"></i>
                             </a>
+                            <a href="#" v-if="editedbadge.id !== badge.id" v-on:click.stop="onEdit(badge)" data-placement="bottom" rel="tooltip" class="actionIcon"
+                               data-original-title="Edit" v-b-tooltip.hover title="Edit">
+                                <i class="uiIconEdit uiIconLightGray"></i></a>
+                            <a href="#" v-if="editedbadge.id === badge.id" v-on:click.stop="onSave(badge)" data-placement="bottom" rel="tooltip" class="actionIcon"
+                               data-original-title="Edit" v-b-tooltip.hover title="Save">
+                                <i class="uiIconSave uiIconLightGray"></i></a>
+                            <a href="#" v-if="editedbadge.id === badge.id" v-on:click.prevent="onCancel(badge)" data-placement="bottom" rel="tooltip" class="actionIcon"
+                               data-original-title="Cancel" v-b-tooltip.hover title="Cancel">
+                                <i class="uiIcon uiIconStatus-canceled uiIconLightGray"></i></a>
                         </td>
                     </tr>
                     <tr v-if="!badges.length">
@@ -43,10 +95,8 @@
                         </td>
                     </tr>
                     </tbody>
-
                 </table>
             </b-col>
-
         </b-row>
     </b-container>
 </template>
@@ -61,19 +111,40 @@
     import 'bootstrap-vue/dist/bootstrap-vue.css'
     export default {
         props: ['badges'],
+        data() {
+            return {
+                formErrors: {},
+                selectedFile: undefined,
+                selectedFileName: '',
+                editedbadge : {}
+            }
+        },
+        watch: {
+            'badge.id'() {
+                this.formErrors = {}
+                this.selectedFile = undefined
+                this.selectedFileName = this.badge.imageName
+            }
+        },
         methods: {
             onEdit(badge) {
-                this.$emit('edit', badge)
+                this.editedbadge=badge;
+                //this.$emit('edit', badge)
             },
             onSave(badge) {
                 this.$emit('save', badge);
                 this.editedbadge= {};
             },
+
+            onImageChanged(event) {
+                this.selectedFile = event.target.files[0]
+                this.selectedFileName = event.target.files[0].name
+            },
             onCancel(badge) {
-                this.editedbadge= {};
+                this.editedbadge= {}
             },
             onRemove(id, title) {
-                this.$emit('remove', id, title)
+                this.$emit('remove', id, title);
             }
         }
     }
@@ -120,5 +191,110 @@
     }
     .uiGrid.table {
         border: none;
+    }
+
+    /* switch test */
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 185px;
+        height: 66px;
+        zoom: 30%;
+        top: 0.4rem;
+    }
+    .switch input {display:none;}
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #f2f2f2;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
+    .slider:before {
+        position: absolute;
+        z-index: 2;
+        content: "";
+        height: 45px;
+        width: 45px;
+        left: 10px;
+        bottom: 11px;
+        background-color: darkgrey;
+        -webkit-box-shadow: 0 2px 5px rgba(0, 0, 0, 0.22);
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.22);
+        -webkit-transition: .4s;
+        transition: all 0.4s ease-in-out;
+    }
+    .slider:after {
+        position: absolute;
+        left: 0;
+        z-index: 1;
+        content: "YES";
+        font-size: 37px;
+        text-align: left !important;
+        line-height: 65px;
+        padding-left: 0;
+        width: 185px;
+        height: 66px !important;
+        color: #f9f9f9;
+        background-color: #477ab3;
+        background-image: -moz-linear-gradient(top, #578dc9, #2f5e92);
+        background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#578dc9), to(#2f5e92));
+        background-image: -webkit-linear-gradient(top, #578dc9, #2f5e92);
+        background-image: -o-linear-gradient(top, #578dc9, #2f5e92);
+        background-image: linear-gradient(to bottom, #578dc9, #2f5e92);
+        background-repeat: repeat-x;
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff578dc9', endColorstr='#ff2f5e92', GradientType=0);
+        -webkit-box-shadow: inset 0px 3px 5px #224469;
+        -moz-box-shadow: inset 0px 3px 5px #224469;
+        box-shadow: inset 0px 3px 5px #224469;
+        -webkit-border-top-left-radius: 9px;
+        -moz-border-radius-topleft: 9px;
+        border-top-left-radius: 9px;
+        -webkit-border-bottom-left-radius: 9px;
+        -moz-border-radius-bottomleft: 9px;
+        border-bottom-left-radius: 9px;
+        height: 57px;
+        border-radius: 100px;
+        background-color: #578dc9;
+        -webkit-transform: translateX(-190px);
+        -ms-transform: translateX(-190px);
+        transform: translateX(-190px);
+        transition: all 0.4s ease-in-out;
+    }
+    input:checked + .slider:after {
+        -webkit-transform: translateX(0px);
+        -ms-transform: translateX(0px);
+        transform: translateX(0px);
+        padding-left: 25px;
+    }
+    input:checked + .slider:before {
+        background-color: #fff;
+    }
+    input:checked + .slider:before {
+        -webkit-transform: translateX(115px);
+        -ms-transform: translateX(115px);
+        transform: translateX(115px);
+    }
+    /* Rounded sliders */
+    .slider.round {
+        border-radius: 100px;
+    }
+    .slider.round:before {
+        border-radius: 50%;
+    }
+    .absolute-no {
+        position: absolute;
+        left: 0;
+        color: darkgrey;
+        text-align: right !important;
+        font-size: 45px;
+        width: calc(100% - 25px);
+        line-height: 70px;
+        cursor: pointer;
     }
 </style>
