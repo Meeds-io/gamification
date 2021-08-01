a9a2e43eaf46559487c979a87eb279e11e22170b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-01 22:59:55 +0100

    TASK-47234 Defer loading of cacheable widgets to load principal application in page first (#123)

--- portlets/src/main/frontend/src/apps/badgesOverview/main.js
+++ portlets/src/main/frontend/src/apps/badgesOverview/main.js
@@ -26,8 +26,6 @@ if (extensionRegistry) {
   }
 }
 
-document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
-
 Vue.use(Vuetify);
 
 const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
@@ -50,9 +48,6 @@ export function init() {
     .then(i18n => {
       // init Vue app when locale ressources are ready
       new Vue({
-        mounted() {
-          document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
-        },
         template: `<badges-overview id='${appId}' v-cacheable="{cacheId: '${cacheId}'}" />`,
         i18n,
         vuetify,
