cda07e9c711bb9e5eef74e9c469f2c19ae8d5d27
Montassar-Hamadi <92787967+Montassar-Hamadi@users.noreply.github.com>
2021-12-21 14:14:14 +0100

    Task-52257: wrong gamification points for like a comment (#178)
    Problem: Both  user1 and user2 received a gamification point of Activity stream (Network): like comment.
    Fix: the gamification points should be :1-User1:  Activity stream (space) like comment.
                                            2-User2  Activity stream (space) receive like comment.

--- services/src/main/java/org/exoplatform/addons/gamification/listener/social/activity/GamificationActivityListener.java
+++ services/src/main/java/org/exoplatform/addons/gamification/listener/social/activity/GamificationActivityListener.java
@@ -262,20 +262,21 @@ public class GamificationActivityListener extends ActivityListenerPlugin {
 
     createActivityGamificationHistoryEntry(activity.getPosterId(),
                                            liker,
-                                           isSpaceActivity ? GAMIFICATION_SOCIAL_LIKE_COMMENT_SPACE_STREAM
-                                                           : GAMIFICATION_SOCIAL_LIKE_COMMENT_NETWORK_STREAM,
+                                           isSpaceActivity ? GAMIFICATION_SOCIAL_LIKE_COMMENT_SPACE_STREAM_TARGET
+                                                           : GAMIFICATION_SOCIAL_LIKE_COMMENT_NETWORK_STREAM_TARGET,
                                            activityUrl);
 
     // a user like a comment made by another user on the stream of other user
     createActivityGamificationHistoryEntry(liker,
                                            activity.getPosterId(),
-                                           GAMIFICATION_SOCIAL_LIKE_COMMENT,
+                                           isSpaceActivity ? GAMIFICATION_SOCIAL_LIKE_COMMENT_SPACE_STREAM
+                                                           : GAMIFICATION_SOCIAL_LIKE_COMMENT_NETWORK_STREAM,
                                            activityUrl);
 
     if (space != null) {
       createSpaceGamificationHistoryEntry(space.getPrettyName(),
                                           liker,
-                                          GAMIFICATION_SOCIAL_LIKE_COMMENT,
+                                          GAMIFICATION_SOCIAL_LIKE_COMMENT_SPACE_STREAM_TARGET,
                                           activityUrl);
     }
   }
