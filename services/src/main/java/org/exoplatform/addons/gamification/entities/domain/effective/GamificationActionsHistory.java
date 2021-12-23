9396786c7a5eefbdb9d32df12ebe571fc9bee9e8
IlhemEssaadi <iessaadi@exoplatform.com>
2021-12-23 12:12:31 +0100

    TASK-52243 : In Statistics Profile gadget, Labels are displayed in bad format

--- services/src/main/java/org/exoplatform/addons/gamification/entities/domain/effective/GamificationActionsHistory.java
+++ services/src/main/java/org/exoplatform/addons/gamification/entities/domain/effective/GamificationActionsHistory.java
@@ -92,11 +92,11 @@ import org.exoplatform.commons.api.persistence.ExoEntity;
         "     ORDER BY total DESC"),
     @NamedQuery(name = "GamificationActionsHistory.findStatsByUser", query = "SELECT"
         +
-        " new org.exoplatform.addons.gamification.service.effective.PiechartLeaderboard(g.domain,SUM(g.actionScore))"
+        " new org.exoplatform.addons.gamification.service.effective.PiechartLeaderboard(g.domainEntity.title,SUM(g.actionScore))"
         +
         " FROM GamificationActionsHistory g" +
         " WHERE g.earnerId = :earnerId" +
-        "     GROUP BY  g.domain"),
+        "     GROUP BY  g.domainEntity.title"),
     @NamedQuery(name = "GamificationActionsHistory.findStatsByUserByDates", query = "SELECT"
         +
         " new org.exoplatform.addons.gamification.service.effective.PiechartLeaderboard(g.domain,SUM(g.actionScore))"
