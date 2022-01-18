7008b533b2c7dd0e017c932884a83d4faee71abf
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-01-18 21:29:43 +0100

    TASK-53277~53275: update actionHistory entity with announcementEntity and DTO model (#198)

---
+++ services/src/main/java/org/exoplatform/addons/gamification/Utils/Utils.java
@@ -0,0 +1,94 @@
+package org.exoplatform.addons.gamification.Utils;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.exoplatform.addons.gamification.service.configuration.DomainService;
+import org.exoplatform.addons.gamification.service.configuration.RuleService;
+import org.exoplatform.addons.gamification.service.dto.configuration.DomainDTO;
+import org.exoplatform.addons.gamification.service.dto.configuration.RuleDTO;
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.security.ConversationState;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
+
+public class Utils {
+
+  private static final Log              LOG                = ExoLogger.getLogger(Utils.class);
+
+  public static final DateTimeFormatter RFC_3339_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss[.SSS][XXX]")
+                                                                              .withResolverStyle(ResolverStyle.LENIENT);
+
+  public static Identity getIdentityByTypeAndId(String type, String name) {
+    IdentityManager identityManager = CommonsUtils.getService(IdentityManager.class);
+    return identityManager.getOrCreateIdentity(type, name);
+  }
+
+  public static final String getCurrentUser() {
+    return ConversationState.getCurrent().getIdentity().getUserId();
+  }
+
+  public static final boolean canEditChallenge(List<Long> managersId) {
+    Identity identity = getIdentityByTypeAndId(OrganizationIdentityProvider.NAME, getCurrentUser());
+
+    return managersId.stream().anyMatch(i -> i == Long.parseLong(identity.getId()));
+  }
+
+  public static final boolean canAnnounce(String id) {
+    SpaceService spaceService = CommonsUtils.getService(SpaceService.class);
+    Space space = spaceService.getSpaceById(id);
+    if (space == null) {
+      throw new IllegalArgumentException("space is not exist");
+    }
+    return spaceService.hasRedactor(space) ? spaceService.isRedactor(space, getCurrentUser())
+        || spaceService.isManager(space, getCurrentUser()) : spaceService.isMember(space, getCurrentUser());
+  }
+
+  public static String toRFC3339Date(Date dateTime) {
+    if (dateTime == null) {
+      return null;
+    }
+    ZonedDateTime zonedDateTime = ZonedDateTime.from(dateTime.toInstant().atOffset(ZoneOffset.UTC));
+    return zonedDateTime.format(RFC_3339_FORMATTER);
+  }
+
+  public static Date parseRFC3339Date(String dateString) {
+    if (StringUtils.isBlank(dateString)) {
+      return null;
+    }
+    ZonedDateTime zonedDateTime = ZonedDateTime.parse(dateString, RFC_3339_FORMATTER);
+    return Date.from(zonedDateTime.toInstant());
+  }
+
+  public static Space getSpaceById(String spaceId) {
+    if (StringUtils.isBlank(spaceId)) {
+      return null;
+    }
+    Space space = CommonsUtils.getService(SpaceService.class).getSpaceById(spaceId);
+    if (space == null) {
+      throw new IllegalArgumentException("space is not exist");
+    }
+    return space;
+  }
+
+  public static DomainDTO getDomainByTitle(String domainTitle) {
+    DomainService domainService = CommonsUtils.getService(DomainService.class);
+    return domainService.findDomainByTitle(domainTitle);
+  }
+
+  public static RuleDTO getRuleById(long ruleId) {
+    RuleService ruleService = CommonsUtils.getService(RuleService.class);
+    return ruleService.findRuleById(ruleId);
+  }
+}
