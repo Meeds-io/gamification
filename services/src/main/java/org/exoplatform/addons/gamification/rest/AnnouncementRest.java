ece8b064b1a1da1e0bc2a56590a454e280266f00
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-01-26 17:34:59 +0100

    TASK-53273:add announcement services and API (#208)

---
+++ services/src/main/java/org/exoplatform/addons/gamification/rest/AnnouncementRest.java
@@ -0,0 +1,102 @@
+package org.exoplatform.addons.gamification.rest;
+
+import io.swagger.annotations.*;
+import org.apache.commons.lang3.StringUtils;
+import org.exoplatform.addons.gamification.service.AnnouncementService;
+import org.exoplatform.addons.gamification.service.dto.configuration.Announcement;
+import org.exoplatform.addons.gamification.service.mapper.EntityMapper;
+import org.exoplatform.addons.gamification.utils.Utils;
+import org.exoplatform.common.http.HTTPStatus;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.rest.resource.ResourceContainer;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/gamification/announcement/api")
+@Api(value = "/gamification/announcement/api", description = "Manages announcement associated to users") // NOSONAR
+@RolesAllowed("users")
+public class AnnouncementRest implements ResourceContainer {
+
+  private static final Log    LOG = ExoLogger.getLogger(AnnouncementRest.class);
+
+  private AnnouncementService announcementService;
+
+  public AnnouncementRest(AnnouncementService announcementService) {
+    this.announcementService = announcementService;
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @RolesAllowed("users")
+  @Path("addAnnouncement")
+  @ApiOperation(value = "Creates a new Announcement", httpMethod = "POST", response = Response.class, consumes = "application/json")
+  @ApiResponses(value = { @ApiResponse(code = HTTPStatus.NO_CONTENT, message = "Request fulfilled"),
+      @ApiResponse(code = HTTPStatus.BAD_REQUEST, message = "Invalid query input"),
+      @ApiResponse(code = HTTPStatus.UNAUTHORIZED, message = "Unauthorized operation"),
+      @ApiResponse(code = HTTPStatus.INTERNAL_ERROR, message = "Internal server error"), })
+  public Response createAnnouncement(@ApiParam(value = "Announcement object to create", required = true)
+  Announcement announcement) {
+    if (announcement == null) {
+      return Response.status(Response.Status.BAD_REQUEST).entity("challenge object is mandatory").build();
+    }
+    String currentUser = Utils.getCurrentUser();
+    if (StringUtils.isBlank(currentUser)) {
+      LOG.warn("current User is null");
+      return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+    try {
+      Announcement newAnnouncement = announcementService.createAnnouncement(announcement, currentUser);
+      return Response.ok(EntityMapper.fromAnnouncement(newAnnouncement)).build();
+    } catch (IllegalAccessException e) {
+      LOG.warn("User '{}' attempts to create an announcement", e);
+      return Response.status(Response.Status.UNAUTHORIZED).entity(e.getMessage()).build();
+    } catch (Exception e) {
+      LOG.warn("Error creating an announcement", e);
+      return Response.serverError().entity(e.getMessage()).build();
+    }
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @RolesAllowed("users")
+  @Path("ByChallengeId/{challengeId}")
+  @ApiOperation(value = "Retrieves the list of challenges available for an owner", httpMethod = "GET", response = Response.class, produces = "application/json")
+  @ApiResponses(value = { @ApiResponse(code = HTTPStatus.OK, message = "Request fulfilled"),
+      @ApiResponse(code = HTTPStatus.UNAUTHORIZED, message = "Unauthorized operation"),
+      @ApiResponse(code = HTTPStatus.INTERNAL_ERROR, message = "Internal server error"), })
+  public Response getAllAnnouncementByChallenge(@ApiParam(value = "id of the challenge", required = true)
+  @PathParam("challengeId")
+  String challengeId,
+                                                @ApiParam(value = "Offset of result", required = false)
+                                                @DefaultValue("0")
+                                                @QueryParam("offset")
+                                                int offset,
+                                                @ApiParam(value = "Limit of result", required = false)
+                                                @DefaultValue("10")
+                                                @QueryParam("limit")
+                                                int limit) {
+    if (offset < 0) {
+      return Response.status(Response.Status.BAD_REQUEST).entity("Offset must be 0 or positive").build();
+    }
+    if (limit <= 0) {
+      return Response.status(Response.Status.BAD_REQUEST).entity("Limit must be positive").build();
+    }
+    try {
+      List<Announcement> announcements = announcementService.findAllAnnouncementByChallenge(Long.parseLong(challengeId),
+                                                                                            offset,
+                                                                                            limit);
+      return Response.ok(EntityMapper.fromAnnouncementList(announcements)).build();
+    } catch (Exception e) {
+      LOG.warn("Error retrieving list of announcements", e);
+      return Response.serverError().entity(e.getMessage()).build();
+    }
+  }
+
+}
--- services/src/main/java/org/exoplatform/addons/gamification/service/configuration/AnnouncementServiceImpl.java
+++ services/src/main/java/org/exoplatform/addons/gamification/service/configuration/AnnouncementServiceImpl.java
@@ -5,10 +5,13 @@ import org.exoplatform.addons.gamification.service.dto.configuration.Announcemen
 import org.exoplatform.addons.gamification.service.dto.configuration.Challenge;
 import org.exoplatform.addons.gamification.storage.AnnouncementStorage;
 import org.exoplatform.addons.gamification.storage.ChallengeStorage;
+import org.exoplatform.addons.gamification.utils.Utils;
 import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.services.listener.ListenerService;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
 
 import java.util.List;
 
@@ -31,6 +34,47 @@ public class AnnouncementServiceImpl implements AnnouncementService {
   }
 
   @Override
+  public Announcement createAnnouncement(Announcement announcement, String currentUser) throws IllegalArgumentException,
+                                                                                        ObjectNotFoundException,
+                                                                                        IllegalAccessException {
+    if (announcement == null) {
+      throw new IllegalArgumentException("announcement is mandatory");
+    }
+    if (announcement.getId() != 0) {
+      throw new IllegalArgumentException("announcement id must be equal to 0");
+    }
+    Challenge challenge = challengeStorage.getChallengeById(announcement.getChallengeId());
+    if (challenge == null) {
+      throw new ObjectNotFoundException("challenge does not exist");
+    }
+    if (announcement.getAssignee() == null) {
+      throw new IllegalArgumentException("announcement assignee must have at least one winner");
+    }
+    if (!Utils.canAnnounce(String.valueOf(challenge.getAudience()))) {
+      throw new IllegalAccessException("user is not allowed to announce challenge");
+    }
+
+    Identity identity = Utils.getIdentityByTypeAndId(OrganizationIdentityProvider.NAME, currentUser);
+    announcement.setCreator(Long.parseLong(identity.getId()));
+    announcement = announcementStorage.saveAnnouncement(announcement);
+    return announcement;
+  }
+
+  @Override
+  public List<Announcement> findAllAnnouncementByChallenge(long challengeId,
+                                                           int offset,
+                                                           int limit) throws ObjectNotFoundException {
+    if (challengeId <= 0) {
+      throw new IllegalArgumentException("Challenge id has to be positive integer");
+    }
+    Challenge challenge = challengeStorage.getChallengeById(challengeId);
+    if (challenge == null) {
+      throw new ObjectNotFoundException("challenge does not exist");
+    }
+    return announcementStorage.findAllAnnouncementByChallenge(challengeId, offset, limit);
+  }
+
+  @Override
   public Long countAllAnnouncementsByChallenge(long challengeId) throws ObjectNotFoundException {
     if (challengeId <= 0) {
       throw new IllegalArgumentException("Challenge id has to be positive integer");
@@ -43,15 +87,26 @@ public class AnnouncementServiceImpl implements AnnouncementService {
   }
 
   @Override
-  public List<Announcement> findAllAnnouncementByChallenge(long challengeId, int offset, int limit) throws  ObjectNotFoundException {
-    if (challengeId <= 0) {
-      throw new IllegalArgumentException("Challenge id has to be positive integer");
+  public Announcement updateAnnouncement(Announcement announcement) throws IllegalArgumentException, ObjectNotFoundException {
+    if (announcement == null) {
+      throw new IllegalArgumentException("announcement is mandatory");
     }
-    Challenge challenge = challengeStorage.getChallengeById(challengeId);
-    if (challenge == null) {
-      throw new ObjectNotFoundException("challenge does not exist");
+    if (announcement.getId() == 0) {
+      throw new IllegalArgumentException("announcement id must not be equal to 0");
     }
-    return announcementStorage.findAllAnnouncementByChallenge(challengeId, offset, limit);
+
+    Announcement oldAnnouncement = announcementStorage.getAnnouncementById(announcement.getId());
+    if (oldAnnouncement == null) {
+      throw new ObjectNotFoundException("Announcement does not exist");
+    }
+    return announcementStorage.saveAnnouncement(announcement);
   }
 
+  @Override
+  public Announcement getAnnouncementById(Long announcementId) throws IllegalArgumentException {
+    if (announcementId == null || announcementId <= 0) {
+      throw new IllegalArgumentException("announcement id is mandatory");
+    }
+    return announcementStorage.getAnnouncementById(announcementId);
+  }
 }
