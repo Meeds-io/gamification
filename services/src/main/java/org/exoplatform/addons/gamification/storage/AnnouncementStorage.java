a4804f620695714025ecbfa22cfb9b529c1f6938
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-01-26 17:34:59 +0100

    TASK-53273:add announcement services and API (#208)

--- services/src/main/java/org/exoplatform/addons/gamification/storage/AnnouncementStorage.java
+++ services/src/main/java/org/exoplatform/addons/gamification/storage/AnnouncementStorage.java
@@ -1,28 +1,76 @@
 package org.exoplatform.addons.gamification.storage;
 
+import org.exoplatform.addons.gamification.IdentityType;
+import org.exoplatform.addons.gamification.entities.domain.configuration.DomainEntity;
 import org.exoplatform.addons.gamification.entities.domain.effective.GamificationActionsHistory;
 import org.exoplatform.addons.gamification.service.dto.configuration.Announcement;
+import org.exoplatform.addons.gamification.service.mapper.DomainMapper;
 import org.exoplatform.addons.gamification.service.mapper.EntityMapper;
 import org.exoplatform.addons.gamification.storage.dao.GamificationHistoryDAO;
+import org.exoplatform.addons.gamification.entities.domain.configuration.RuleEntity;
+import org.exoplatform.addons.gamification.service.dto.configuration.Challenge;
+import org.exoplatform.addons.gamification.utils.Utils;
 
+import java.util.Date;
 import java.util.List;
 
 public class AnnouncementStorage {
 
-  private GamificationHistoryDAO gamificationHistoryDAO;
+  private GamificationHistoryDAO announcementDAO;
 
+  private ChallengeStorage       challengeStorage;
 
-  public AnnouncementStorage(GamificationHistoryDAO gamificationHistoryDAO) {
-    this.gamificationHistoryDAO = gamificationHistoryDAO;
+  public static final long       MILLIS_IN_A_DAY = 1000 * 60 * 60 * 24; // NOSONAR
+
+  public AnnouncementStorage(GamificationHistoryDAO announcementDAO, ChallengeStorage challengeStorage) {
+    this.announcementDAO = announcementDAO;
+    this.challengeStorage = challengeStorage;
   }
 
-  public Long countAnnouncementsByChallenge(Long challengeId) {
-    Long countAnnounce = gamificationHistoryDAO.countAnnouncementsByChallenge(challengeId);
-    return countAnnounce;
+  public Announcement saveAnnouncement(Announcement announcement) {
+    if (announcement == null) {
+      throw new IllegalArgumentException("Announcement argument is null");
+    }
+    Challenge challenge = challengeStorage.getChallengeById(announcement.getChallengeId());
+    RuleEntity challengeEntity = EntityMapper.toEntity(challenge);
+    GamificationActionsHistory announcementEntity = EntityMapper.toEntity(announcement);
+    Date nextToEndDate = new Date(challengeEntity.getEndDate().getTime() + MILLIS_IN_A_DAY);
+
+    if (!announcementEntity.getCreatedDate().before(nextToEndDate)) {
+      throw new IllegalArgumentException("announcement is not allowed when challenge is ended ");
+    }
+    if (!announcementEntity.getCreatedDate().after(challengeEntity.getStartDate())) {
+      throw new IllegalArgumentException("announcement is not allowed when challenge is not started ");
+    }
+    // TODO to be changed after adding score and domain to challenge
+    DomainEntity domainEntity = DomainMapper.domainDTOToDomain(Utils.getDomainByTitle("social"));
+    announcementEntity.setEarnerType(IdentityType.USER);
+    announcementEntity.setActionTitle(challengeEntity.getTitle());
+    announcementEntity.setActionScore(20);
+    announcementEntity.setGlobalScore(Utils.getUserGlobalScore(String.valueOf(announcement.getAssignee())));
+    announcementEntity.setDomainEntity(domainEntity);
+    announcementEntity.setDomain(domainEntity.getTitle());
+    if (announcementEntity.getId() == null) {
+      announcementEntity = announcementDAO.create(announcementEntity);
+    } else {
+      announcementEntity = announcementDAO.update(announcementEntity);
+    }
+    return EntityMapper.fromEntity(announcementEntity);
+  }
+
+  public Announcement getAnnouncementById(long announcementId) {
+    GamificationActionsHistory announcementEntity = this.announcementDAO.find(announcementId);
+    return EntityMapper.fromEntity(announcementEntity);
   }
 
   public List<Announcement> findAllAnnouncementByChallenge(Long challengeId, int offset, int limit) {
-    List<GamificationActionsHistory> announcementEntities= gamificationHistoryDAO.findAllAnnouncementByChallenge(challengeId, offset, limit);
+    List<GamificationActionsHistory> announcementEntities = announcementDAO.findAllAnnouncementByChallenge(challengeId,
+                                                                                                           offset,
+                                                                                                           limit);
     return EntityMapper.fromAnnouncementEntities(announcementEntities);
+  }
+
+  public Long countAnnouncementsByChallenge(Long challengeId) {
+    return announcementDAO.countAnnouncementsByChallenge(challengeId);
   }
 }
