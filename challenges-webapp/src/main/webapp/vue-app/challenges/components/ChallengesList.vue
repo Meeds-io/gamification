f3c2f945f026e14e9e029163972bd7330186fb02
Mohamed El Hadi Rdid <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2022-01-26 17:33:00 +0100

    Task-53480 : Add challenge webapp in gamification (#209)

---
+++ challenges-webapp/src/main/webapp/vue-app/challenges/components/ChallengesList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row no-gutters>
+        <v-col
+          v-for="challenge in challenges"
+          :key="challenge.id"
+          class="mb-4"
+          cols="12"
+          md="6"
+          lg="4"
+          xl="3">
+          <challenge-card :challenge="challenge" @edit="$emit('edit-challenge',challenge)" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  props: {
+    challenges: {
+      type: Object,
+      default: function() {
+        return [];
+      },
+    }
+  },
+};
+</script>
