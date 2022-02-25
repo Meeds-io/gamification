4e6d4d82abe0b2efa16ec23db9d6fd8a44a5c8d9
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-02-25 10:40:44 +0100

    TASK-54643: fix editing challenge with empty program and ability to edit challenge (#263)

--- challenges-webapp/src/main/webapp/vue-app/challenges/components/ChallengeCard.vue
+++ challenges-webapp/src/main/webapp/vue-app/challenges/components/ChallengeCard.vue
@@ -196,7 +196,6 @@ export default {
         createDate: announcement.createdDate
       };
       this.listWinners.unshift(newAnnouncement);
-      this.$refs.winnersDetails.listWinners.unshift(newAnnouncement);
       this.challenge.announcementsCount = this.challenge.announcementsCount +1;
     },
     openDetails() {
