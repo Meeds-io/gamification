1091785ec3067a6030b1cf92e96c1cddec2b6acc
GouadriaHanen <hgouadria@exoplatform.com>
2022-06-16 11:24:58 +0100

    MEED-281: fix displaying old announcements activities

--- challenges-webapp/src/main/webapp/vue-app/challenges-extensions/challengesExtensions.js
+++ challenges-webapp/src/main/webapp/vue-app/challenges-extensions/challengesExtensions.js
@@ -24,13 +24,12 @@ export function initExtensions() {
     getThumbnail: () => '/challenges/images/challengesAppIcon.png',
     getSummary: activity => activity && activity.templateParams && activity.templateParams.announcementChallenge  || activity && activity.templateParams && activity.templateParams.announcementDescription  || '',
     getBody: activity => {
-      const body = activity.title || Vue.prototype.$utils.trim((activity.templateParams && activity.templateParams.announcementComment)
-           || '');
+      const body = activity.templateParams?.announcementComment && Vue.prototype.$utils.trim(activity.templateParams?.announcementComment) || activity.title || '';
       return body.includes('<oembed>') && body.split('<oembed>')[0] || body;
     },
     getBodyToEdit: activity => {
-      return activity.title || Vue.prototype.$utils.trim((activity.templateParams && activity.templateParams.announcementComment)
-           || '');
+      const bodyToEdit = activity.templateParams?.announcementComment && Vue.prototype.$utils.trim(activity.templateParams?.announcementComment) || activity.title;
+      return Vue.prototype.$utils.trim(window.decodeURIComponent(bodyToEdit));
     },
   };
 
