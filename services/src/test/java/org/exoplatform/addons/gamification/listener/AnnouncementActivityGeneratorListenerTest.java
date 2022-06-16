6a27de89e695fbb4b0239b8a271b856dcb391816
GouadriaHanen <hgouadria@exoplatform.com>
2022-06-16 17:48:59 +0100

    Add license header

--- services/src/test/java/org/exoplatform/addons/gamification/listener/AnnouncementActivityGeneratorListenerTest.java
+++ services/src/test/java/org/exoplatform/addons/gamification/listener/AnnouncementActivityGeneratorListenerTest.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2022 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.exoplatform.addons.gamification.listener;
 
 import org.exoplatform.addons.gamification.listener.challenges.AnnouncementActivityGeneratorListener;
@@ -13,7 +29,6 @@ import org.exoplatform.container.ExoContainer;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
 import org.exoplatform.social.core.identity.model.Identity;
-import org.exoplatform.social.core.manager.ActivityManager;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.storage.api.ActivityStorage;
 import org.exoplatform.social.websocket.ActivityStreamWebSocketService;
