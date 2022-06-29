d4e183b236cfbca1e426c58c8d21935ab53648b5
GouadriaHanen <hgouadria@exoplatform.com>
2022-06-29 17:22:47 +0100

    fix no search result text

--- challenges-webapp/src/main/webapp/vue-app/challenges/components/NoResultFound.vue
+++ challenges-webapp/src/main/webapp/vue-app/challenges/components/NoResultFound.vue
@@ -5,11 +5,11 @@
         src="/challenges/images/challenge_not_found_filter_image.png"
         :alt="$t('challenges.search.noResults')"
         width="230px"
-        class="mx-auto mb-2 searchNoResultsImg"
+        class="mx-auto mb-2"
         loading="lazy">
-      <div class="d-flex flex-column ma-auto text-center text-sub-title">
-        <span class="title">{{ $t('challenges.search.noResults') }}</span>
-        <span class="text-sub-title subtitle-1">{{ $t('challenges.search.noResultsMessage') }}</span>
+      <div class="d-flex flex-column text-center text-sub-title">
+        <span class="text-header-title text-light-color">{{ $t('challenges.search.noResults') }}</span>
+        <span class="text-sub-title subtitle-1 text-light-color">{{ $t('challenges.search.noResultsMessage') }}</span>
       </div>
     </div>
   </v-flex>
