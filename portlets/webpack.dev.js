f7c288ef48a8844f4f70a01d30e26010115df6ab
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-09-05 09:45:48 +0100

    Update portlets/webpack.dev.js

--- portlets/webpack.dev.js
+++ portlets/webpack.dev.js
@@ -21,7 +21,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   output: {
-      path: `/home/exo/work/exoRepos/Reward2FT/meedsBuilders/meeds-community-1.4.x-meedsv2-SNAPSHOT/webapps/gamification-portlets/`,
+      path: `/exo-server/webapps/gamification-portlets/`,
   },
   mode: 'development',
 });
