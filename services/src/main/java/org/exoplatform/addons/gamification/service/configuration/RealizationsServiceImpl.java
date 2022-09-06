b53a89c42c8d17871fef3bc3c565796d2025f927
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-09-06 16:54:40 +0100

    MEED-523: Changing achievements report exporting type to xlsx  (#447)
    This change is going to change Typo from `xls` into `xlsx`, while the xlsx format has been resolved in the task [MEED-195](https://github.com/Meeds-io/gamification/pull/440) where `XSSFBook` has been used to write data into an `xlsx` file

--- services/src/main/java/org/exoplatform/addons/gamification/service/configuration/RealizationsServiceImpl.java
+++ services/src/main/java/org/exoplatform/addons/gamification/service/configuration/RealizationsServiceImpl.java
@@ -117,7 +117,7 @@ public class RealizationsServiceImpl implements RealizationsService {
   }
 
   @Override
-  public InputStream exportXls(String fileName,
+  public InputStream exportXlsx(String fileName,
                                List<GamificationActionsHistoryRestEntity> gamificationActionsHistoryRestEntities) throws IOException {
     String data = stringifyAchievements(gamificationActionsHistoryRestEntities);
     String[] dataToWrite = data.split("\\r?\\n");
@@ -183,7 +183,7 @@ public class RealizationsServiceImpl implements RealizationsService {
         sbResult.append(DELIMITER);
         sbResult.append(SEPARATOR);
       } catch (Exception e) {
-        LOG.error("Error when computing to XLS ", e);
+        LOG.error("Error when computing to XLSX ", e);
       }
     });
     return sbResult.toString();
