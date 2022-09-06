c0df824c328f7051117ec4512bf69c3f188096cf
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-09-06 09:58:26 +0100

    MEED-195: add the ability to show achievements depending on the userType. (#440)
    this change is going to get the realizations from Realizations Table depending on the membership that the user own; "/platform/administrators" would be able to access ALL the [realizations](https://community.exoplatform.com/portal/dw/drives?documentPreviewId=ab9255f5c0a8a01105868162b8abfea1) .
    
    Co-authored-by: Boubaker Khanfir <boubaker.khanfir@exoplatform.com>

--- services/src/main/java/org/exoplatform/addons/gamification/service/RealizationsService.java
+++ services/src/main/java/org/exoplatform/addons/gamification/service/RealizationsService.java
@@ -1,11 +1,15 @@
 package org.exoplatform.addons.gamification.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 import org.exoplatform.addons.gamification.service.dto.configuration.GamificationActionsHistoryDTO;
+import org.exoplatform.addons.gamification.service.dto.configuration.GamificationActionsHistoryRestEntity;
 import org.exoplatform.addons.gamification.service.dto.configuration.RealizationsFilter;
 import org.exoplatform.addons.gamification.service.dto.configuration.constant.HistoryStatus;
 import org.exoplatform.commons.exception.ObjectNotFoundException;
-
-import java.util.List;
+import org.exoplatform.services.security.Identity;
 
 public interface RealizationsService {
 
@@ -13,13 +17,16 @@ public interface RealizationsService {
    * Retrieves all Realizations by Filter.
    *
    * @param filter used to filter realizations using {@link RealizationsFilter}
+   * @param current identity {@link Identity}
    * @param offset Offset
    * @param limit Limit
    * @return A {@link List <GamificationActionsHistoryDTO>} object
+   * @throws IllegalAccessException when User doesn't have enough privileges to access achievements of user
    */
-  List<GamificationActionsHistoryDTO> getAllRealizationsByFilter(RealizationsFilter filter,
-                                                                 int offset,
-                                                                 int limit);
+  List<GamificationActionsHistoryDTO> getRealizationsByFilter(RealizationsFilter filter,
+                                                              Identity identity,
+                                                              int offset,
+                                                              int limit) throws IllegalAccessException;
 
   /**
    * Retrieves all Realizations by Date.
@@ -30,4 +37,20 @@ public interface RealizationsService {
    *           technical identifier is not found
    */
   GamificationActionsHistoryDTO updateRealizationStatus(Long gHistoryId, HistoryStatus status, String actionLabel, Long points, String domain) throws ObjectNotFoundException;
+
+  /**
+   * compute xls from all Realizations .
+   *
+   * @param A {@link List <GamificationActionsHistoryDTO>} object
+   * @param A filename String 
+   * @return A xls InputStream {@link InputStream}
+   * @throws IOException
+   * @throws ObjectNotFoundException GamificationActionsHistory identified by
+   *           its technical identifier is not found
+   */
+  InputStream exportXls(String fileName,
+                   List<GamificationActionsHistoryRestEntity> gamificationActionsHistoryRestEntities) throws IllegalAccessException,
+                                                                                                      IOException;
+  
 }
+
