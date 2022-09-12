da6f9d277efb6582bb710e4d39fd698f47cfd309
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-09-12 13:56:37 +0100

    MEED-112: implement add program feature  (#459)
    prior to this change, only from the programs administration page
    programs could be added.
    after this change, adding programs is available on the engagement center
    programs tab

---
+++ portlets/src/main/webapp/vue-app/engagement-center/components/EngagementCenter.vue
@@ -0,0 +1,65 @@
+<!--
+This file is part of the Meeds project (https://meeds.io/).
+Copyright (C) 2022 Meeds Association
+contact@meeds.io
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-app>
+    <main v-if="engagementCenterEnabled">
+      <v-tabs
+        id="engagementCenterTabs"
+        v-model="tab"
+        slider-size="4"
+        class="mb-4">
+        <v-tab class="px-5">{{ $t('engagementCenter.label.programs') }}</v-tab>
+        <v-tab class="px-5">{{ $t('engagementCenter.label.challenges') }}</v-tab>
+        <v-tab class="px-5">{{ $t('engagementCenter.label.achievements') }}</v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <engagement-center-programs id="engagementCenterProgramsTab" />
+        </v-tab-item>
+        <v-tab-item>
+          <challenges />
+        </v-tab-item>
+        <v-tab-item>
+          <realizations
+            id="Realizations"
+            :earner-id="earnerId"
+            :is-administrator="isAdministrator"
+            :retrieve-all="isAdministrator" />
+        </v-tab-item>
+      </v-tabs-items>
+    </main>
+    <main v-else>
+      <challenges />
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    isAdministrator: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: () => ({
+    engagementCenterEnabled: eXo.env.portal.engagementCenterEnabled,
+    tab: null,
+    earnerId: eXo.env.portal.userIdentityId,
+  }),
+};
+</script>
--- services/src/main/java/org/exoplatform/addons/gamification/service/configuration/DomainServiceImpl.java
+++ services/src/main/java/org/exoplatform/addons/gamification/service/configuration/DomainServiceImpl.java
@@ -16,22 +16,30 @@
  */
 package org.exoplatform.addons.gamification.service.configuration;
 
-import org.exoplatform.addons.gamification.entities.domain.configuration.DomainEntity;
-import org.exoplatform.addons.gamification.service.dto.configuration.DomainDTO;
-import org.exoplatform.addons.gamification.service.mapper.DomainMapper;
-import org.exoplatform.addons.gamification.storage.DomainStorage;
-import org.exoplatform.addons.gamification.utils.Utils;
-import org.exoplatform.commons.exception.ObjectNotFoundException;
-import org.exoplatform.commons.utils.CommonsUtils;
-import org.exoplatform.services.listener.ListenerService;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
-
-import javax.persistence.EntityNotFoundException;
+import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
+import org.exoplatform.addons.gamification.service.dto.configuration.DomainDTO;
+import org.exoplatform.addons.gamification.service.dto.configuration.DomainFilter;
+import org.exoplatform.addons.gamification.service.dto.configuration.constant.EntityType;
+import org.exoplatform.addons.gamification.storage.DomainStorage;
+import org.exoplatform.addons.gamification.utils.Utils;
+import org.exoplatform.commons.exception.ObjectNotFoundException;
+import org.exoplatform.commons.file.model.FileItem;
+import org.exoplatform.commons.file.services.FileService;
+import org.exoplatform.services.listener.ListenerService;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.security.Identity;
+import org.exoplatform.services.security.IdentityConstants;
+import org.exoplatform.social.core.manager.IdentityManager;
+
 public class DomainServiceImpl implements DomainService {
+
+  public static final String      GAMIFICATION_DOMAIN_LISTENER = "exo.gamification.domain.action";
 
   private static final Log        LOG                          = ExoLogger.getLogger(DomainServiceImpl.class);
 
@@ -39,125 +47,190 @@ public class DomainServiceImpl implements DomainService {
 
   protected final ListenerService listenerService;
 
-  public static final String      GAMIFICATION_DOMAIN_LISTENER = "exo.gamification.domain.action";
+  protected final IdentityManager identityManager;
 
-  public DomainServiceImpl() {
-    this.domainStorage = CommonsUtils.getService(DomainStorage.class);
-    this.listenerService = CommonsUtils.getService(ListenerService.class);
+  protected final FileService     fileService;
 
+  public DomainServiceImpl(DomainStorage domainStorage,
+                           ListenerService listenerService,
+                           IdentityManager identityManager,
+                           FileService fileService) {
+    this.domainStorage = domainStorage;
+    this.listenerService = listenerService;
+    this.identityManager = identityManager;
+    this.fileService = fileService;
   }
 
-  public List<DomainDTO> getAllDomains() {
-    try {
-      // --- load all Domains
-      return domainStorage.getAllDomains();
-    } catch (Exception e) {
-      LOG.error("Error to find Domains", e);
-      throw (e);
-    }
+  @Override
+  public List<DomainDTO> getAllDomains(DomainFilter filter, int offset, int limit) {
+    return domainStorage.getAllDomains(filter, offset, limit);
   }
 
+  @Override
   public List<DomainDTO> getEnabledDomains() {
-    try {
-      // --- load all Domains
-      return domainStorage.getEnabledDomains();
-
-    } catch (Exception e) {
-      LOG.error("Error to find Domains", e);
-      throw (e);
-    }
+    return domainStorage.getEnabledDomains();
   }
 
+  @Override
   public DomainDTO findEnabledDomainByTitle(String domainTitle) {
-    try {
-      return domainStorage.findEnabledDomainByTitle(domainTitle);
-
-    } catch (Exception e) {
-      LOG.error("Error to find Domain entity with title : {}", domainTitle, e);
-      throw (e);
+    if (StringUtils.isBlank(domainTitle)) {
+      throw new IllegalArgumentException("domainTitle has to be not null");
     }
-
+    return domainStorage.findEnabledDomainByTitle(domainTitle);
   }
 
+  @Override
   public DomainDTO getDomainByTitle(String domainTitle) {
+    if (StringUtils.isBlank(domainTitle)) {
+      throw new IllegalArgumentException("domainTitle has to be not null");
+    }
+    return domainStorage.getDomainByTitle(domainTitle);
+  }
+
+  @Override
+  public int countDomains(DomainFilter domainFilter) {
+    return domainStorage.countDomains(domainFilter);
+  }
+
+  @Override
+  public DomainDTO createDomain(DomainDTO domain, Identity aclIdentity) throws IllegalAccessException {
+    if (domain == null) {
+      throw new IllegalArgumentException("domain is mandatory");
+    }
+    if (domain.getId() != 0) {
+      throw new IllegalArgumentException("domain id must be equal to 0");
+    }
+    if (!canAddDomain(aclIdentity)) {
+      throw new IllegalAccessException("The user is not authorized to create a domain");
+    }
+    return createDomain(domain, aclIdentity.getUserId());
+  }
+
+  @Override
+  public DomainDTO createDomain(DomainDTO domain) {
+    return createDomain(domain, IdentityConstants.SYSTEM);
+  }
+
+  @Override
+  public DomainDTO updateDomain(DomainDTO domain, Identity aclIdentity) throws IllegalAccessException, ObjectNotFoundException {
+    if (!canUpdateDomain(domain.getId(), aclIdentity)) {
+      throw new IllegalAccessException("The user is not authorized to update domain " + domain);
+    }
+    DomainDTO storedDomain = domainStorage.getDomainById(domain.getId());
+    if (storedDomain == null) {
+      throw new ObjectNotFoundException("domain doesn't exist");
+    } else if (domain.equals(storedDomain)) {
+      // No changes so no modifications needed
+      return storedDomain;
+    } else if (storedDomain.isDeleted()) {
+      throw new IllegalAccessException("Domain is already marked as deleted");
+    }
+    domain.setLastModifiedBy(aclIdentity.getUserId());
+    domain.setLastModifiedDate(Utils.toRFC3339Date(new Date(System.currentTimeMillis())));
+
+    // Preserve non modifiable attributes
+    domain.setType(storedDomain.getType());
+    domain.setCreatedBy(storedDomain.getCreatedBy());
+    domain.setCreatedDate(storedDomain.getCreatedDate());
+    domain.setDeleted(storedDomain.isDeleted());
+    domain.setCoverFileId(storedDomain.getCoverFileId());
+
+    domain = domainStorage.saveDomain(domain);
+    if (storedDomain.isEnabled() && !domain.isEnabled()) {
+      broadcast(domain, "disable");
+    }
+    if (!storedDomain.isEnabled() && domain.isEnabled()) {
+      broadcast(domain, "enable");
+    }
+    return domain;
+  }
+
+  @Override
+  public void deleteDomain(long id, Identity aclIdentity) throws IllegalAccessException, ObjectNotFoundException {
+    if (!canUpdateDomain(id, aclIdentity)) {
+      throw new IllegalAccessException("The user is not authorized to create a domain");
+    }
+    String date = Utils.toRFC3339Date(new Date(System.currentTimeMillis()));
+    DomainDTO domain = domainStorage.getDomainById(id);
+    if (domain == null) {
+      throw new ObjectNotFoundException("domain doesn't exist");
+    }
+    domain.setDeleted(true);
+    domain.setLastModifiedDate(date);
+    domain = domainStorage.saveDomain(domain);
+    broadcast(domain, "delete");
+  }
+
+  @Override
+  public DomainDTO getDomainById(long domainId) {
+    if (domainId <= 0) {
+      throw new IllegalArgumentException("domain id has to be positive integer");
+    }
+    return domainStorage.getDomainById(domainId);
+  }
+
+  @Override
+  public InputStream getFileDetailAsStream(long domainId) throws ObjectNotFoundException {
+    DomainDTO domain = domainStorage.getDomainById(domainId);
+    if (domain == null) {
+      throw new ObjectNotFoundException("Domain with id " + domainId + " doesn't exist");
+    }
+    if (domain.getCoverFileId() == 0) {
+      throw new ObjectNotFoundException("Domain with id " + domainId + " doesn't have a coverdId");
+    }
+    FileItem fileItem;
     try {
-      return domainStorage.getDomainByTitle(domainTitle);
-
+      fileItem = fileService.getFile(domain.getCoverFileId());
+      return fileItem == null || fileItem.getFileInfo() == null ? null : fileItem.getAsStream();
     } catch (Exception e) {
-      LOG.error("Error to find Domain entity with title : {}", domainTitle, e);
-      throw (e);
-    }
-
-  }
-
-  public DomainDTO addDomain(DomainDTO domainDTO) {
-    DomainDTO oldDomainDTO = null;
-    String date = Utils.toRFC3339Date(new Date(System.currentTimeMillis()));
-    try {
-      oldDomainDTO = domainStorage.findEnabledDomainByTitle(domainDTO.getTitle());
-      if (oldDomainDTO == null) {
-        domainDTO.setCreatedDate(date);
-        domainDTO.setLastModifiedDate(date);
-        domainDTO = domainStorage.saveDomain(domainDTO);
-      } else if (oldDomainDTO.isDeleted()) {
-        domainDTO.setId(oldDomainDTO.getId());
-        domainDTO.setLastModifiedDate(date);
-        domainDTO = domainStorage.saveDomain(domainDTO);
-      }
-    } catch (Exception e) {
-      LOG.error("Error to create domain with title {}", domainDTO.getTitle(), e);
-      throw (e);
-    }
-    return domainDTO;
-  }
-
-  public DomainDTO updateDomain(DomainDTO domainDTO) throws ObjectNotFoundException {
-    String date = Utils.toRFC3339Date(new Date(System.currentTimeMillis()));
-    DomainDTO oldDomainDTO = domainStorage.getDomainByID(domainDTO.getId());
-    if (oldDomainDTO != null) {
-      domainDTO.setLastModifiedDate(date);
-      domainDTO = domainStorage.saveDomain(domainDTO);
-      DomainEntity domainEntity = DomainMapper.domainDTOToDomain(domainDTO);
-      try {
-        if (oldDomainDTO.isEnabled() && !domainDTO.isEnabled()) {
-          listenerService.broadcast(GAMIFICATION_DOMAIN_LISTENER, domainEntity, "disable");
-        }
-        if (!oldDomainDTO.isEnabled() && domainDTO.isEnabled()) {
-          listenerService.broadcast(GAMIFICATION_DOMAIN_LISTENER, domainEntity, "enable");
-        }
-      } catch (Exception e) {
-        LOG.error("Error to update domain {} with title {}", domainDTO.getId(), domainDTO.getTitle(), e);
-      }
-      return domainDTO;
-    } else {
-      throw new ObjectNotFoundException("domain is not exist");
-    }
-  }
-
-  public void deleteDomain(Long id) throws EntityNotFoundException {
-    String date = Utils.toRFC3339Date(new Date(System.currentTimeMillis()));
-    DomainDTO domainDTO = domainStorage.getDomainByID(id);
-    if (domainDTO != null) {
-      domainDTO.setDeleted(true);
-      domainDTO.setLastModifiedDate(date);
-      domainDTO = domainStorage.saveDomain(domainDTO);
-      try {
-        DomainEntity domainEntity = DomainMapper.domainDTOToDomain(domainDTO);
-        listenerService.broadcast(GAMIFICATION_DOMAIN_LISTENER, domainEntity, "delete");
-      } catch (Exception e) {
-        LOG.error("Error to delete domain {}", id, e);
-      }
-    } else {
-      LOG.warn("Domain {} not Found", id);
-      throw (new EntityNotFoundException());
+      LOG.warn("Error retrieving image with id {}", domainId, e);
+      return null;
     }
   }
 
   @Override
-  public DomainDTO getDomainById(Long domainId) {
-    if (domainId <= 0) {
-      throw new IllegalArgumentException("domain id has to be positive integer");
-    }
-    return domainStorage.getDomainByID(domainId);
+  public boolean canAddDomain(Identity aclIdentity) {
+    return isAdministrator(aclIdentity);
   }
+
+  @Override
+  public boolean canUpdateDomain(long domainId, Identity aclIdentity) {
+    if (isAdministrator(aclIdentity)) {
+      return true;
+    } else if (aclIdentity == null) {
+      return false;
+    }
+    DomainDTO domain = domainStorage.getDomainById(domainId);
+    org.exoplatform.social.core.identity.model.Identity userIdentity =
+                                                                     identityManager.getOrCreateUserIdentity(aclIdentity.getUserId());
+    return domain != null && userIdentity != null && domain.getOwners() != null
+        && domain.getOwners().contains(Long.parseLong(userIdentity.getId()));
+  }
+
+  private boolean isAdministrator(org.exoplatform.services.security.Identity identity) {
+    return identity != null && identity.isMemberOf("/platform/administrators");
+  }
+
+  private void broadcast(DomainDTO domain, String operation) {
+    try {
+      listenerService.broadcast(GAMIFICATION_DOMAIN_LISTENER, domain, operation);
+    } catch (Exception e) {
+      LOG.warn("Error while broadcasting operation '{}' on domain {}. The operation '{}' isn't interrupted.",
+               operation,
+               domain,
+               e);
+    }
+  }
+
+  private DomainDTO createDomain(DomainDTO domain, String username) {
+    if (StringUtils.isBlank(domain.getType())) {
+      domain.setType(EntityType.AUTOMATIC.name());
+    }
+    domain.setCreatedBy(username);
+    domain.setCreatedDate(Utils.toRFC3339Date(new Date(System.currentTimeMillis())));
+    domain.setLastModifiedBy(username);
+    domain.setLastModifiedDate(Utils.toRFC3339Date(new Date(System.currentTimeMillis())));
+    return domainStorage.saveDomain(domain);
+  }
+
 }
