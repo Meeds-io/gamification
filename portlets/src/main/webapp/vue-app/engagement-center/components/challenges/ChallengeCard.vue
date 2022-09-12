97dc27434c235e57a029bed4c34240902814450a
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-09-12 16:40:57 +0100

    feat: Add Engagement Center Edit Program Drawer - MEED-114 - Meeds-io/meeds#13 (#461)
    prior to this change, the manual program could not be edited
    after this change, the program owner is able to edit his programs

--- portlets/src/main/webapp/vue-app/engagement-center/components/challenges/ChallengeCard.vue
+++ portlets/src/main/webapp/vue-app/engagement-center/components/challenges/ChallengeCard.vue
@@ -15,7 +15,7 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
-  <div class="challenge-card">
+  <div class="engagement-center-card">
     <v-card
       id="engagementCenterChallengeCard"
       class="mx-auto card cardOfChallenge"
