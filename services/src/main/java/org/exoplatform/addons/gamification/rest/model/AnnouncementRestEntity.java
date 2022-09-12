3cc7bff7ce8bc360235b828b2e20a0b2f10f7eb2
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-09-12 13:56:37 +0100

    MEED-112: implement add program feature  (#459)
    prior to this change, only from the programs administration page
    programs could be added.
    after this change, adding programs is available on the engagement center
    programs tab

---
+++ services/src/main/java/org/exoplatform/addons/gamification/rest/model/AnnouncementRestEntity.java
@@ -0,0 +1,61 @@
+package org.exoplatform.addons.gamification.rest.model;
+
+public class AnnouncementRestEntity implements Cloneable {
+
+  private long     id;
+
+  private String   assignee;
+
+  private String   createdDate;
+
+  private Long     activityId;
+
+  public AnnouncementRestEntity(long id,
+                                String assignee,
+                                String createdDate,
+                                Long activityId) {
+    this.id = id;
+    this.assignee = assignee;
+    this.createdDate = createdDate;
+    this.activityId = activityId;
+  }
+  @Override
+  public AnnouncementRestEntity clone() { // NOSONAR
+    return new AnnouncementRestEntity(id, assignee, createdDate, activityId);
+  }
+
+  public AnnouncementRestEntity() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getAssignee() {
+    return assignee;
+  }
+
+  public void setAssignee(String assignee) {
+    this.assignee = assignee;
+  }
+
+  public String getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(String createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Long getActivityId() {
+    return activityId;
+  }
+
+  public void setActivityId(Long activityId) {
+    this.activityId = activityId;
+  }
+}
