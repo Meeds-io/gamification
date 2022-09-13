a3a05f9ad7b93a92fb139e0fd3e11124273381b1
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2022-09-13 11:39:26 +0100

    feat: add  engagement center programs quick filter - MEED-499 - Meeds-io/meeds#13 (#464)
    prior to this change, filtring programs are not implemented after this
    change, we can filter programs with their status: ENABLED, DISABLED or
    ALL

--- portlets/src/main/webapp/vue-app/engagement-center/components/common/NoResultFound.vue
+++ portlets/src/main/webapp/vue-app/engagement-center/components/common/NoResultFound.vue
@@ -15,7 +15,7 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
-  <v-flex class="searchNoResults justify-center d-flex border-box-sizing">
+  <v-flex class="searchNoResults justify-center d-flex border-box-sizing emptyChallenges">
     <div class="d-flex flex-column">
       <img
         src="/gamification-portlets/skin/images/challenge_not_found_filter_image.png"
@@ -24,9 +24,24 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
         class="mx-auto mb-2"
         loading="lazy">
       <div class="d-flex flex-column text-center text-sub-title">
-        <span class="text-header-title text-light-color">{{ $t('challenges.search.noResults') }}</span>
-        <span class="text-sub-title subtitle-1 text-light-color">{{ $t('challenges.search.noResultsMessage') }}</span>
+        <span v-if="info" class="text-header-title text-light-color">{{ info }}</span>
+        <span v-if="infoMessage" class="text-sub-title subtitle-1 text-light-color">{{ infoMessage }}</span>
       </div>
     </div>
   </v-flex>
 </template>
+<script>
+
+export default {
+  props: {
+    info: {
+      type: String,
+      default: ''
+    },
+    infoMessage: {
+      type: String,
+      default: ''
+    },
+  },
+};
+</script>
