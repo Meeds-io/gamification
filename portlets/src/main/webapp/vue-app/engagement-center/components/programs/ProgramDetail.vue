4533d00a99201b79762dcea593f34e8924d10df9
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-09-27 18:00:04 +0100

    feat: Engagement center Open program detail by clicking on program card - MEED-417 - Meeds-io/MIPs#13 (#494)
    Fix feedbacks

--- portlets/src/main/webapp/vue-app/engagement-center/components/programs/ProgramDetail.vue
+++ portlets/src/main/webapp/vue-app/engagement-center/components/programs/ProgramDetail.vue
@@ -62,7 +62,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
           <v-list-item-subtitle class="text-color pt-4">
             <v-data-table
               :headers="rulesHeaders"
-              :items="programRules"
+              :items="programRulesToDisplay"
               :options.sync="options"
               :server-items-length="totalSize"
               :no-data-text="$t('programs.details.rules.noRules')"
@@ -125,6 +125,9 @@ export default {
     };
   },
   computed: {
+    programRulesToDisplay() {
+      return this.programRules.filter(rule => !rule.deleted);
+    },
     programTitle() {
       return this.program?.title;
     },
@@ -170,7 +173,7 @@ export default {
       }
       const offset = (page - 1) * itemsPerPage;
       this.loadingRules = true;
-      return this.$ruleServices.getRules(null, this.programId, 'ENABLED', offset, itemsPerPage)
+      return this.$ruleServices.getRules(null, this.programId, 'ENABLED', 'ALL', offset, itemsPerPage)
         .then((data) => {
 
           this.programRules = data.rules;
