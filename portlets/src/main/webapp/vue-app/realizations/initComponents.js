80df69a322fac433530ea185023f6ac091e83561
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-10-18 16:51:18 +0100

    feat: Engagement Center Add advanced filter for realization table to filter by programs - MEED-139 - Meeds-io/MIPs#13 (#536)
    to make the realizations table more significant for users, the
    realizations should be filtred by programs .
    this change is going to add the possiblity to the user to filter
    realizations by the programs
    
    Co-authored-by: Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>

--- portlets/src/main/webapp/vue-app/realizations/initComponents.js
+++ portlets/src/main/webapp/vue-app/realizations/initComponents.js
@@ -1,20 +1,32 @@
 import Realizations from './components/Realizations.vue';
 import RealizationItem from './components/RealizationItem.vue';
 import SelectPeriod from './components/SelectPeriod.vue';
+import NoResultFound from '../engagement-center/components/common/NoResultFound.vue';
 import EditRealizationDrawer  from './components/EditRealizationDrawer.vue';
+import FilterRealizationsDrawer from './components/FilterRealizationsDrawer.vue';
+import FilterRealizationsProgramList from './components/FilterRealizationsProgramList.vue';
 
 const components = {
   'realizations': Realizations,
   'realization-item': RealizationItem,
   'select-period': SelectPeriod,
   'edit-realization-drawer': EditRealizationDrawer,
+  'filter-realizations-drawer': FilterRealizationsDrawer,
+  'realizations-filter-program-list': FilterRealizationsProgramList,
+  'engagement-center-no-results': NoResultFound,
 };
 
 for (const key in components) {
   Vue.component(key, components[key]);
 }
 import  * as realizationsServices  from './realizationsServices';
+import  * as programsServices  from '../engagement-center/js/programsServices';
 
+if (!Vue.prototype.$programsServices) {
+  window.Object.defineProperty(Vue.prototype, '$programsServices', {
+    value: programsServices,
+  });
+}
 if (!Vue.prototype.$realizationsServices) {
   window.Object.defineProperty(Vue.prototype, '$realizationsServices', {
     value: realizationsServices,
