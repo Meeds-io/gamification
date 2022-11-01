5bc532744cf8124b143003137a39d10c50818d3d
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-11-01 14:41:32 +0100

    feat: Implement overview page my reputation portlet- MEED-665 - Meeds-io/MIPs#10 (#547)
    this change is going to implement myReputation portlet
    
    Co-authored-by: Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>

--- portlets/src/main/webapp/vue-app/badgesOverview/components/BadgesOverview.vue
+++ portlets/src/main/webapp/vue-app/badgesOverview/components/BadgesOverview.vue
@@ -19,10 +19,16 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
     :class="owner && 'profileBadge' || 'profileBadgeOther'"
     class="white">
     <v-toolbar
+      id="badgesOverviewHeader"
       color="white"
       flat
-      class="border-box-sizing">
-      <div class="text-header-title text-sub-title">
+      class="border-box-sizing"
+      :height= "isOverviewDisplay ? '50px' : 'auto'"
+      >
+      <div v-if="isOverviewDisplay" class="subtitle-2 align-self-start my-0">
+        {{ $t('gamification.myReputation.badgesTitle') }}
+      </div>
+      <div v-else class="text-header-title text-sub-title">
         {{ $t('exoplatform.gamification.badgesByDomain') }}
       </div>
     </v-toolbar>
@@ -47,6 +53,12 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 <script>
 export default {
+  props: {
+    isOverviewDisplay: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
   data: () => ({
     badges: [],
   }),
--- portlets/src/main/webapp/vue-app/myReputation/components/myReputation.vue
+++ portlets/src/main/webapp/vue-app/myReputation/components/myReputation.vue
@@ -16,44 +16,31 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
   <v-card
-    :loading="loading"
     height="338"
-    width="30%"
-    min-width="350"
     flat>
-    <div class="d-flex flex-row pa-4">
-      <h4 class="hidden-space my-0"> {{ $t('overview.myContributions.title') }} </h4>
-      <v-spacer />
-      <a v-if="seeAllDisplay" :href="achievementsURL"> <h5 class="text-font-size primary--text my-0"> {{ $t('overview.myContributions.seeAll') }} </h5> </a>
+    <div class="d-flex flex-row px-4 pt-4">
+      <h4 class="hidden-space my-0"> {{ $t('gamification.myReputation.title') }} </h4>
     </div>
-    <div class="mt-n2">
-      <user-points-widget
-        :overview-display="true"
-        @seeAll="updateDisplaySeeAll($event)"
-        @loadingData="updateLoading($event)" />
+    <div>
+      <div class="d-flex flex-row px-4 pt-4">
+        <h4 class="subtitle-2 align-self-start my-0 black--text"> {{ $t('gamification.myReputation.KudosTitle') }} </h4>
+      </div>
+      <extension-registry-components
+        :params="params"
+        name="my-reputation-overview"
+        type="my-reputation-item"
+        class="d-flex flex-column" />
     </div>
   </v-card>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      seeAllDisplay: false,
-      loading: true
-    };
-  },
   computed: {
-    achievementsURL() {
-      return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/contributions/achievements`;
+    params() {
+      return {
+        isOverviewDisplay: true,
+      };
     }
-  },
-  methods: {
-    updateDisplaySeeAll(value) {
-      this.seeAllDisplay = value;
-    },
-    updateLoading(value) {
-      this.loading = value;
-    },
   }
 };
 </script>
