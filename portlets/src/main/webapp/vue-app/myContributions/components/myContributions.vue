a0634afd4d740f8a4828fb8bfca64376997f6468
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2022-11-02 16:52:44 +0100

    feat: Overview Page Add a description in contributions portlet when nothing to display - MEED-663 - Meeds-io/MIPs#10 (#563)
    This change will add a new box containing an icon with a description in the contributions portlet when there is nothing to display.

--- portlets/src/main/webapp/vue-app/myContributions/components/myContributions.vue
+++ portlets/src/main/webapp/vue-app/myContributions/components/myContributions.vue
@@ -16,6 +16,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
   <v-card
+    :loading="loading"
     height="280"
     width="30%"
     min-width="350"
@@ -23,19 +24,36 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
     <div class="d-flex flex-row pa-4">
       <h4 class="hidden-space my-0"> {{ $t('overview.myContributions.title') }} </h4>
       <v-spacer />
-      <a :href="achievementsURL"> <h5 class="text-font-size primary--text my-0"> {{ $t('overview.myContributions.seeAll') }} </h5> </a>
+      <a v-if="seeAllDisplay" :href="achievementsURL"> <h5 class="text-font-size primary--text my-0"> {{ $t('overview.myContributions.seeAll') }} </h5> </a>
     </div>
     <div class="mt-n2">
-      <user-points-widget :overview-display="true" />
+      <user-points-widget
+        :overview-display="true"
+        @seeAll="updateDisplaySeeAll($event)"
+        @loadingData="updateLoading($event)" />
     </div>
   </v-card>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      seeAllDisplay: false,
+      loading: true
+    };
+  },
   computed: {
     achievementsURL() {
       return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/contributions/achievements`;
     }
   },
+  methods: {
+    updateDisplaySeeAll(value) {
+      this.seeAllDisplay = value;
+    },
+    updateLoading(value) {
+      this.loading = value;
+    },
+  }
 };
 </script>
