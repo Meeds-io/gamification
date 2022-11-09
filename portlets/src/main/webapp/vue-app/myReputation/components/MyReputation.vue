5efac071dfbc48870c2c42b45dd89e4436e30c6b
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-11-09 14:04:02 +0100

    feat: Implement portlets in Overview Page  - MEED-665 - MEED-551 - MEED-667 - Meeds-io/MIPs#10  (#575)
    this change is going to Implement overview page portlets .
    
    Co-authored-by: Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
    Co-authored-by: Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
    Co-authored-by: MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>

---
+++ portlets/src/main/webapp/vue-app/myReputation/components/MyReputation.vue
@@ -0,0 +1,48 @@
+<!--
+This file is part of the Meeds project (https://meeds.io/).
+Copyright (C) 2022 Meeds Association
+contact@meeds.io
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <gamification-overview-widget>
+    <template #title>
+      {{ $t('gamification.myReputation.title') }}
+    </template>
+    <template #content>
+      <gamification-overview-widget-row>
+        <template #title>
+          {{ $t('gamification.myReputation.KudosTitle') }}
+        </template>
+        <template #content>
+          <extension-registry-components
+            :params="params"
+            name="my-reputation-overview"
+            type="my-reputation-item"
+            class="d-flex flex-column mx-n4 mt-n4" />
+        </template>
+      </gamification-overview-widget-row>
+    </template>
+  </gamification-overview-widget>
+</template>
+<script>
+export default {
+  computed: {
+    params() {
+      return {
+        isOverviewDisplay: true,
+      };
+    }
+  }
+};
+</script>
---
+++ portlets/src/main/webapp/vue-app/programsOverview/components/ProgramsOverview.vue
@@ -0,0 +1,61 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-app>
+    <gamification-overview-widget>
+      <template #title>
+        {{ $t('gamification.overview.programsOverviewTitle') }}
+      </template>
+      <template #content>
+        <gamification-overview-widget-row class="my-auto">
+          <template #icon>
+            <v-icon color="secondary" size="55px">fas fa-bullhorn</v-icon>
+          </template>
+          <template #content>
+            <span v-html="emptySummaryText"></span>
+          </template>
+        </gamification-overview-widget-row>
+      </template>
+    </gamification-overview-widget>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    emptyActionName: 'gamification-programsOverview-check-action',
+  }),
+  computed: {
+    emptySummaryText() {
+      return this.$t('gamification.overview.programsOverviewSummary', {
+        0: `<a href="javascript:void(0)" onclick="document.dispatchEvent(new CustomEvent('${this.emptyActionName}'))">`,
+        1: '</a>',
+      });
+    },
+  },
+  created() {
+    document.addEventListener(this.emptyActionName, this.clickOnEmptyActionLink);
+  },
+  beforeDestroy() {
+    document.removeEventListener(this.emptyActionName, this.clickOnEmptyActionLink);
+  },
+  methods: {
+    clickOnEmptyActionLink() {
+      // TODO
+      console.warn('clickOnEmptyActionLink');//eslint-disable-line no-console
+    },
+  },
+};
+</script>
