294aca1df6b36adc4b5d15ab289b519233c77707
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-11-09 14:04:02 +0100

    feat: Implement portlets in Overview Page  - MEED-665 - MEED-551 - MEED-667 - Meeds-io/MIPs#10  (#575)
    this change is going to Implement overview page portlets .
    
    Co-authored-by: Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
    Co-authored-by: Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
    Co-authored-by: MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>

---
+++ portlets/src/main/webapp/vue-app/topContributors/components/TopContributors.vue
@@ -0,0 +1,34 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-app>
+    <gamification-overview-widget>
+      <template #title>
+        {{ $t('gamification.overview.topContributorsTitle') }}
+      </template>
+      <template #content>
+        <gamification-overview-widget-row class="my-auto">
+          <template #icon>
+            <v-icon color="secondary" size="55px">fas fa-trophy</v-icon>
+          </template>
+          <template #content>
+            <span v-html="$t('gamification.overview.topContributorsSummary')"></span>
+          </template>
+        </gamification-overview-widget-row>
+      </template>
+    </gamification-overview-widget>
+  </v-app>
+</template>
