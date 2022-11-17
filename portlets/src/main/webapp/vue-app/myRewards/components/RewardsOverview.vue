48df85289fa630a363053391e3a8359060f7ce59
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2022-11-17 17:18:43 +0100

    feat: Overview Page Fix program name display in the pie chart - Meed 1096 - Meeds-io/MIPs#10 (#604)
    This change will fix the display when no contributions.

--- portlets/src/main/webapp/vue-app/myRewards/components/RewardsOverview.vue
+++ portlets/src/main/webapp/vue-app/myRewards/components/RewardsOverview.vue
@@ -75,7 +75,7 @@ export default {
       };
     },
     walletURL() {
-      return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/wallet`;
+      return this.rewardDisplayed ? `${eXo.env.portal.context}/${eXo.env.portal.portalName}/wallet` : '';
     }
   },
   created() {
