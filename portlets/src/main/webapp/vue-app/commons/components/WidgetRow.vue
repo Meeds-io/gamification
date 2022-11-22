b5601ceb53bde7f1a59ed0ac2a781e4e25c5a695
ArfaouiMohamedAmine <marfaoui@exoplatform.com>
2022-11-22 10:37:27 +0100

    MEED-1102 fix redirection passing for only those who are provided with one

--- portlets/src/main/webapp/vue-app/commons/components/WidgetRow.vue
+++ portlets/src/main/webapp/vue-app/commons/components/WidgetRow.vue
@@ -3,7 +3,7 @@
     <div v-if="$slots.title" class="subtitle-2">
       <slot name="title"></slot>
     </div>
-    <v-list-item class="px-0" @click="selectedRowUrl()">
+    <v-list-item class="px-0" v-on="redirectionUrl ? { click: selectedRowUrl } : {}">
       <v-list-item-action-text v-if="$slots.icon" class="me-4">
         <slot name="icon"></slot>
       </v-list-item-action-text>
