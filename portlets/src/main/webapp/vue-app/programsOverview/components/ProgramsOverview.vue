77e305893bf44bf4e14bc231e3e1bfb235f06f42
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2022-11-29 14:31:01 +0100

    feat: Overview Page Display the programs portlet - MEED-674 - Meeds-io/MIPs#10  (#635)
    This change will add the programs portlet positionned in the sixth position. It contains the title of the portlet and three listed programs.

--- portlets/src/main/webapp/vue-app/programsOverview/components/ProgramsOverview.vue
+++ portlets/src/main/webapp/vue-app/programsOverview/components/ProgramsOverview.vue
@@ -15,17 +15,53 @@
 -->
 <template>
   <v-app>
-    <gamification-overview-widget>
+    <gamification-overview-widget
+      :see-all-url="programURL"
+      :loading="loading"
+      extraClass="px-0">
       <template #title>
         {{ $t('gamification.overview.programsOverviewTitle') }}
       </template>
       <template #content>
-        <gamification-overview-widget-row class="my-auto">
+        <gamification-overview-widget-row v-show="!programsDisplayed" class="my-auto">
           <template #icon>
             <v-icon color="secondary" size="55px">fas fa-bullhorn</v-icon>
           </template>
           <template #content>
             <span v-html="emptySummaryText"></span>
+          </template>
+        </gamification-overview-widget-row>
+        <gamification-overview-widget-row
+          v-show="programsDisplayed"
+          class="py-auto"                   
+          v-for="(item, index) in programs" 
+          :key="index"
+          :redirection-url="`${programURL}/${item.id}`">
+          <template #content>
+            <span>
+              <v-list
+                class="pb-0"
+                subheader
+                two-line>
+                <v-list-item
+                  two-line>
+                  <v-list-item-icon class="mx-2">
+                    <v-img
+                      :src="item.coverUrl"
+                      height="40"
+                      width="55" />
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ item.title }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle> 
+                      <span class="text-light-color" v-sanitized-html="$t('programs.budget', {0: `<span>${item.rulesTotalScore} ${$t('programs.details.label.points')}</span>`})"></span>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </span>
           </template>
         </gamification-overview-widget-row>
       </template>
@@ -36,6 +72,11 @@
 export default {
   data: () => ({
     emptyActionName: 'gamification-programsOverview-check-action',
+    programs: [],
+    status: 'ENABLED',
+    type: 'ALL',
+    loading: true,
+    programsDisplayed: false
   }),
   computed: {
     emptySummaryText() {
@@ -44,9 +85,13 @@ export default {
         1: '</a>',
       });
     },
+    programURL() {
+      return this.programsDisplayed ? `${eXo.env.portal.context}/${eXo.env.portal.portalName}/contributions/programs` : '';
+    },
   },
   created() {
     document.addEventListener(this.emptyActionName, this.clickOnEmptyActionLink);
+    this.retrievePrograms();
   },
   beforeDestroy() {
     document.removeEventListener(this.emptyActionName, this.clickOnEmptyActionLink);
@@ -54,6 +99,15 @@ export default {
   methods: {
     clickOnEmptyActionLink() {
       window.location.href = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/contributions/challenges`;
+    },
+    retrievePrograms() {
+      return this.$programsServices
+        .retrievePrograms(0, 3, this.type, this.status)
+        .then((data) => {
+          this.programs = data.domains;
+          this.programsDisplayed = data.domainsSize > 0;
+          this.loading = false;
+        });
     },
   },
 };
