af53fd4c8f0558a381056ff63863ad015ba4cfc7
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-11-29 14:39:51 +0100

    feat: Overview Page fix my reputation display - MEED-961 - Meeds-io/MIPs#10  (#633)
    this change is going to fix my reputation portlet display misalignment

--- portlets/src/main/webapp/vue-app/myReputation/components/MyReputation.vue
+++ portlets/src/main/webapp/vue-app/myReputation/components/MyReputation.vue
@@ -20,32 +20,36 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
       {{ $t('gamification.myReputation.title') }}
     </template>
     <template #content>
-      <gamification-overview-widget-row v-show="kudosDisplayed">
-        <template #title>
-          {{ $t('gamification.myReputation.KudosTitle') }}
-        </template>
-        <template #content>
-          <extension-registry-components
-            :params="params"
-            name="my-reputation-overview-kudos"
-            type="my-reputation-item"
-            class="d-flex flex-column mx-n4 mt-n4" />
-        </template>
-      </gamification-overview-widget-row>
-      <gamification-overview-widget-row v-show="!kudosDisplayed">
-        <template #title>
-          <div class="mb-4">
-            {{ $t('gamification.myReputation.KudosTitleNoData') }}
-          </div>
-        </template>
-        <template #icon>
-          <v-icon color="secondary" size="55px">fas fa-award</v-icon>
-        </template>
-        <template #content>
-          <span v-html="emptyKudosSummaryText"></span>
-        </template>
-      </gamification-overview-widget-row>
-      <gamification-overview-widget-row class="mt-n3" v-show="badgesDisplayed">
+      <v-card flat height="94">
+        <gamification-overview-widget-row v-show="kudosDisplayed">
+          <template #title>
+            {{ $t('gamification.myReputation.KudosTitle') }}
+          </template>
+          <template #content>
+            <extension-registry-components
+              :params="params"
+              name="my-reputation-overview-kudos"
+              type="my-reputation-item"
+              class="d-flex flex-column mx-n4" />
+          </template>
+        </gamification-overview-widget-row>
+        <gamification-overview-widget-row v-show="!kudosDisplayed">
+          <template #title>
+            <div class="mb-4">
+              {{ $t('gamification.myReputation.KudosTitleNoData') }}
+            </div>
+          </template>
+          <template #icon>
+            <v-icon color="secondary" size="55px">fas fa-award</v-icon>
+          </template>
+          <template #content>
+            <span v-html="emptyKudosSummaryText"></span>
+          </template>
+        </gamification-overview-widget-row>
+      </v-card>
+      <gamification-overview-widget-row
+        class="my-auto"
+        v-show="badgesDisplayed">
         <template #title>
           <div class="subtitle-2 align-self-start mt-10">
             {{ $t('gamification.myReputation.badgesTitle') }}
@@ -63,8 +67,8 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
         class="my-auto"
         v-show="!badgesDisplayed">
         <template #title>
-          <div class="mb-3 mt-11">
-         {{ $t('gamification.myReputation.badgesTitle') }}
+          <div class="mb-3 mt-1">
+            {{ $t('gamification.myReputation.badgesTitle') }}
           </div>
         </template>
         <template #icon>
