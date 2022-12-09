2da2405399346f3dc0937f6843332f099ca0f5ae
ArfaouiMohamedAmine <marfaoui@exoplatform.com>
2022-12-09 12:28:40 +0100

    MEED-1238: fix

--- services/src/main/java/org/exoplatform/addons/gamification/rest/ChallengeRest.java
+++ services/src/main/java/org/exoplatform/addons/gamification/rest/ChallengeRest.java
@@ -18,7 +18,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.collections.ListUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import org.exoplatform.addons.gamification.rest.model.ChallengeRestEntity;
