b6b8070fd4fd7995f1edf0d3e4e38dbcf35024dd
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-12-13 11:11:14 +0100

    feat: Engagement Center Improvement on achievement table by adding tooltips - MEED-1261 - Meeds-io/MIPs#13 (#687)
    This change is going to add `ToolTips` to :
    
    - date column, redefining it with `hours` and `minutes`
    - Type column, by adding `Automatic` or `Manual`
    - Status column, by adding `Accepted` or `Rejected`

--- portlets/src/main/webapp/vue-app/realizations/components/RealizationItem.vue
+++ portlets/src/main/webapp/vue-app/realizations/components/RealizationItem.vue
@@ -16,6 +16,59 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
   <tr :id="`GamificationRealizationItem${realization.id}`">
+    <td class="wrap">
+      <v-tooltip bottom>
+        <template #activator="{ on, attrs }">
+          <div
+            v-bind="attrs"
+            v-on="on">
+            <date-format
+              :format="dateFormat"
+              :value="realization.createdDate" />
+          </div>
+        </template>
+        <span>           
+          <date-format
+            :format="tooltipDateFormat"
+            :value="realization.createdDate" />
+        </span>
+      </v-tooltip>
+    </td>
+    <td v-if="isAdministrator" class="text-truncate align-center">
+      <exo-user-avatar
+        :identity="earner"
+        :size="28"
+        extra-class="d-inline-block"
+        link-style
+        popover
+        avatar />
+    </td>
+    <td class="text-truncate align-center">
+      <v-tooltip bottom>
+        <template #activator="{ on, attrs }">
+          <v-icon
+            :class="statusIconClass"
+            class="me-1"
+            size="16"
+            v-bind="attrs"
+            v-on="on">
+            {{ actionTypeIcon }}
+          </v-icon>
+        </template>
+        <span>{{ isAutomaticTypeLabel }}</span>
+      </v-tooltip>
+    </td>
+    <td>
+      <v-tooltip bottom>
+        <template #activator="{ on }">
+          <a v-on="on" @click="openProgramDetail">
+            <div class="text-truncate">{{ programTitle }}
+            </div>
+          </a>
+        </template>
+        <span v-html="programTitle"></span>
+      </v-tooltip>
+    </td>
     <td>
       <div v-if="isAutomaticType">
         <extension-registry-component
