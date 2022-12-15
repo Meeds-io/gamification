0a893012ba08deddcb0ad4aecaab6812fd0a1304
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-15 12:23:24 +0100

    feat: Generate a notification when announcement made in Space audience - MEED-1284 - Meeds-io/MIPs#13 (#705)
    Prior to this change, the announcements generates Activities through
    ActivityStorage without triggering listeners to notify Space Members.
    This change will allow to trigger activity generation listeners to
    notify users.
    
    In addition, this will remove usage of `ConversationState.getCurrent`
    inside Listeners.

--- services/src/test/java/org/exoplatform/addons/gamification/listener/AnnouncementActivityGeneratorListenerTest.java
+++ services/src/test/java/org/exoplatform/addons/gamification/listener/AnnouncementActivityGeneratorListenerTest.java
@@ -16,121 +16,154 @@
  */
 package org.exoplatform.addons.gamification.listener;
 
+import static org.exoplatform.addons.gamification.listener.challenges.AnnouncementActivityGeneratorListener.ANNOUNCEMENT_DESCRIPTION_TEMPLATE_PARAM;
+import static org.exoplatform.addons.gamification.listener.challenges.AnnouncementActivityGeneratorListener.ANNOUNCEMENT_ID_TEMPLATE_PARAM;
+import static org.exoplatform.addons.gamification.utils.Utils.ANNOUNCEMENT_ACTIVITY_TYPE;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import org.exoplatform.addons.gamification.listener.challenges.AnnouncementActivityGeneratorListener;
-import org.exoplatform.addons.gamification.rest.EntityBuilder;
 import org.exoplatform.addons.gamification.service.AnnouncementService;
 import org.exoplatform.addons.gamification.service.ChallengeService;
 import org.exoplatform.addons.gamification.service.dto.configuration.Announcement;
 import org.exoplatform.addons.gamification.service.dto.configuration.AnnouncementActivity;
 import org.exoplatform.addons.gamification.service.dto.configuration.Challenge;
 import org.exoplatform.addons.gamification.utils.Utils;
-import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.services.listener.Event;
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
 import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
+import org.exoplatform.social.core.manager.ActivityManager;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.space.model.Space;
-import org.exoplatform.social.core.storage.api.ActivityStorage;
-import org.exoplatform.social.websocket.ActivityStreamWebSocketService;
-import org.exoplatform.social.websocket.entity.ActivityStreamModification;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.exoplatform.social.core.space.spi.SpaceService;
 
-import java.util.Collections;
-import java.util.Date;
-
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
-
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore({ "javax.management.*", "javax.xml.*", "org.xml.*" })
-@PrepareForTest({ EntityBuilder.class, Utils.class })
+@RunWith(MockitoJUnitRunner.class)
 public class AnnouncementActivityGeneratorListenerTest {
 
   @Mock
-  private AnnouncementService            announcementService;
+  private AnnouncementService announcementService;
 
   @Mock
-  private ExoContainer                   container;
+  private ExoContainer        container;
 
   @Mock
-  private ActivityStorage                activityStorage;
+  private SpaceService        spaceService;
 
   @Mock
-  private ChallengeService               challengeService;
+  private ActivityManager     activityManager;
 
   @Mock
-  private ActivityStreamWebSocketService activityStreamWebSocketService;
+  private ChallengeService    challengeService;
 
   @Mock
-  IdentityManager                        identityManager;
+  IdentityManager             identityManager;
 
   @Test
-  public void testGenerateAnnouncementActivity() throws IllegalAccessException, ObjectNotFoundException {
-    PowerMockito.mockStatic(EntityBuilder.class);
-    PowerMockito.mockStatic(Utils.class);
-    AnnouncementActivityGeneratorListener announcementActivityGeneratorListener =
-                                                                                new AnnouncementActivityGeneratorListener(identityManager,
-                                                                                                                          activityStorage,
-                                                                                                                          challengeService,
-                                                                                                                          activityStreamWebSocketService);
+  public void testGenerateAnnouncementActivity() throws Exception {
+    AnnouncementActivityGeneratorListener activityGeneratorListener = new AnnouncementActivityGeneratorListener(spaceService,
+                                                                                                                identityManager,
+                                                                                                                activityManager,
+                                                                                                                challengeService);
+    String spaceId = "5";
+    String userIdentityId = "3";
+    String activityId = "9";
 
-    Challenge challenge = new Challenge(1,
+    Challenge challenge = new Challenge(1234,
                                         "new challenge",
                                         "challenge description",
-                                        1l,
+                                        Long.parseLong(spaceId),
                                         new Date(System.currentTimeMillis()).toString(),
                                         new Date(System.currentTimeMillis() + 1).toString(),
                                         Collections.emptyList(),
                                         10L,
-                                        "gamification",
+                                        2l,
                                         true);
 
-    Announcement announcement = new Announcement(1,
-                                                 challenge.getId(),
-                                                 challenge.getTitle(),
-                                                 1L,
-                                                 "announcement comment",
-                                                 1L,
-                                                 new Date(System.currentTimeMillis()).toString(),
-                                                 null);
-    AnnouncementActivity announcementActivity = new AnnouncementActivity(1,
+    AnnouncementActivity announcementActivity = new AnnouncementActivity(6,
                                                                          challenge.getId(),
                                                                          challenge.getTitle(),
-                                                                         1L,
+                                                                         Long.parseLong(userIdentityId),
                                                                          "announcement comment",
-                                                                         1L,
+                                                                         Long.parseLong(userIdentityId),
                                                                          new Date(System.currentTimeMillis()).toString(),
                                                                          null,
                                                                          null);
+
     Space space = new Space();
-    space.setId("1");
+    space.setId(spaceId);
+    space.setPrettyName("test_space");
     space.setDisplayName("test space");
+
     Identity spaceIdentity = new Identity();
-    spaceIdentity.setId("1");
-    spaceIdentity.setProviderId("space");
-    spaceIdentity.setRemoteId("test_space");
+    spaceIdentity.setId(userIdentityId);
+    spaceIdentity.setProviderId(SpaceIdentityProvider.NAME);
+    spaceIdentity.setRemoteId(space.getPrettyName());
 
-    ExoSocialActivityImpl activity = new ExoSocialActivityImpl();
-    activity.setId("1");
-    when(Utils.getCurrentUser()).thenReturn("root");
-    when(Utils.getSpaceById(String.valueOf(challenge.getAudience()))).thenReturn(space);
-    when(identityManager.getIdentity(anyString())).thenReturn(spaceIdentity);
+    Identity userIdentity = new Identity();
+    userIdentity.setId(userIdentityId);
+    userIdentity.setProviderId(OrganizationIdentityProvider.NAME);
+    userIdentity.setRemoteId("testuser");
 
-    when(challengeService.getChallengeById(anyLong(), anyString())).thenReturn(challenge);
-    when(announcementService.updateAnnouncement(any(Announcement.class))).thenReturn(announcement);
-    when(activityStorage.saveActivity(any(), any())).thenReturn(activity);
-    Event event = new Event<>(Utils.ANNOUNCEMENT_ACTIVITY_EVENT, announcementService, announcementActivity);
-    announcementActivityGeneratorListener.onEvent(event);
-    verify(announcementService, times(1)).updateAnnouncement(any(Announcement.class));
-    verify(challengeService, times(1)).getChallengeById(anyLong(), anyString());
-    verify(activityStreamWebSocketService, times(1)).sendMessage(any(ActivityStreamModification.class));
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(identityManager.getOrCreateSpaceIdentity(space.getPrettyName())).thenReturn(spaceIdentity);
+    when(identityManager.getIdentity(userIdentity.getId())).thenReturn(userIdentity);
+    when(challengeService.getChallengeById(challenge.getId(), userIdentity.getRemoteId())).thenReturn(challenge);
+    when(announcementService.updateAnnouncement(any())).thenAnswer(invocation -> invocation.getArgument(0));
+    doAnswer(invocation -> {
+      ExoSocialActivityImpl activity = invocation.getArgument(1);
+      activity.setId(activityId);
+      when(activityManager.getActivity(activityId)).thenReturn(activity);
+      return null;
+    }).when(activityManager).saveActivityNoReturn(any(), any());
+
+    Event<AnnouncementService, AnnouncementActivity> event = new Event<>(Utils.ANNOUNCEMENT_ACTIVITY_EVENT,
+                                                                         announcementService,
+                                                                         announcementActivity);
+    activityGeneratorListener.onEvent(event);
+    verify(activityManager, times(1)).saveActivityNoReturn(argThat(new ArgumentMatcher<Identity>() {
+      @Override
+      public boolean matches(Identity spaceIdentity) {
+        return StringUtils.equals(space.getPrettyName(), String.valueOf(spaceIdentity.getRemoteId()));
+      }
+    }), argThat(new ArgumentMatcher<ExoSocialActivity>() {
+      @Override
+      public boolean matches(ExoSocialActivity activity) {
+        assertEquals(ANNOUNCEMENT_ACTIVITY_TYPE, activity.getType());
+        assertEquals(announcementActivity.getComment(), activity.getTitle());
+        assertEquals(userIdentityId, activity.getUserId());
+        assertEquals(String.valueOf(announcementActivity.getId()),
+                     activity.getTemplateParams().get(ANNOUNCEMENT_ID_TEMPLATE_PARAM));
+        assertEquals(challenge.getTitle(),
+                     activity.getTemplateParams().get(ANNOUNCEMENT_DESCRIPTION_TEMPLATE_PARAM));
+        return true;
+      }
+    }));
+
+    verify(announcementService, times(1)).updateAnnouncement(argThat(new ArgumentMatcher<Announcement>() {
+      @Override
+      public boolean matches(Announcement announcement) {
+        return StringUtils.equals(activityId, String.valueOf(announcement.getActivityId()));
+      }
+    }));
   }
 
 }
