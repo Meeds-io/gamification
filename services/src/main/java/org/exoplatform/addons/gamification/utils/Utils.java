4c53d84abfa3e499c495a7f5c888b5747e2d8020
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-05 12:59:42 +0100

    feat: Improve Test configuration and use Mockito istead of PowerMock for static mocking - MEED-1451 - Meeds-io/MIPs#42 (#765)
    Due to changes in bottom layers, with configurations cleanup, we are
    able to reduce test configuration and avoid redondance by just importing
    artifacts in pom.xml, this way PortalContainer.getInstance() will not
    raise an exception anymore, even without additional configuration
    settings.

--- services/src/main/java/org/exoplatform/addons/gamification/utils/Utils.java
+++ services/src/main/java/org/exoplatform/addons/gamification/utils/Utils.java
@@ -20,7 +20,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -57,8 +56,6 @@ import org.exoplatform.social.rest.api.EntityBuilder;
 import org.exoplatform.social.rest.entity.IdentityEntity;
 import org.exoplatform.web.security.codec.CodecInitializer;
 import org.exoplatform.web.security.security.TokenServiceInitializationException;
-
-import liquibase.repackaged.org.apache.commons.collections4.CollectionUtils;
 
 public class Utils {
 
@@ -356,7 +353,7 @@ public class Utils {
           return toUserInfo(identity);
         }
         return null;
-      }).filter(Objects::nonNull).collect(Collectors.toList());
+      }).filter(Objects::nonNull).toList();
     } catch (Exception e) {
       LOG.error("Error when getting challenge managers {}", e);
       return Collections.emptyList();
@@ -377,7 +374,7 @@ public class Utils {
           return userInfo;
         }
         return null;
-      }).filter(Objects::nonNull).collect(Collectors.toList());
+      }).filter(Objects::nonNull).toList();
     } catch (Exception e) {
       LOG.error("Error when getting challenge managers {}", e);
       return Collections.emptyList();
@@ -537,7 +534,8 @@ public class Utils {
       return true;
     }
     if (isSpaceMember(spaceId, username)
-        && CollectionUtils.containsAny(ownerIds, Long.parseLong(userIdentity.getId()))) {
+        && ownerIds != null
+        && ownerIds.contains(Long.parseLong(userIdentity.getId()))) {
       return true;
     }
     return isSuperManager(username);
