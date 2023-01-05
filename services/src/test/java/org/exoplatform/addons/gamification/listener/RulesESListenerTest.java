b75f2da8357109a56d39487415c2d16ba1066e9e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-05 12:59:42 +0100

    feat: Improve Test configuration and use Mockito istead of PowerMock for static mocking - MEED-1451 - Meeds-io/MIPs#42 (#765)
    Due to changes in bottom layers, with configurations cleanup, we are
    able to reduce test configuration and avoid redondance by just importing
    artifacts in pom.xml, this way PortalContainer.getInstance() will not
    raise an exception anymore, even without additional configuration
    settings.

--- services/src/test/java/org/exoplatform/addons/gamification/listener/RulesESListenerTest.java
+++ services/src/test/java/org/exoplatform/addons/gamification/listener/RulesESListenerTest.java
@@ -17,19 +17,23 @@
 
 package org.exoplatform.addons.gamification.listener;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import org.exoplatform.addons.gamification.listener.es.RulesESListener;
 import org.exoplatform.addons.gamification.service.dto.configuration.RuleDTO;
 import org.exoplatform.addons.gamification.test.AbstractServiceTest;
 import org.exoplatform.addons.gamification.utils.Utils;
 import org.exoplatform.commons.search.index.IndexingService;
 import org.exoplatform.services.listener.Event;
-import org.junit.Test;
-import org.mockito.Mock;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
+@RunWith(MockitoJUnitRunner.class)
 public class RulesESListenerTest extends AbstractServiceTest {
 
   @Mock
