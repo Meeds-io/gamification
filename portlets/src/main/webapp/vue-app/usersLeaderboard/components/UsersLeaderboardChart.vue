345019d0497ec109fbfd4edbae544fbff4d46498
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-26 10:37:39 +0100

    feat: Improve People Pie Chart to hide labels when displayed - MEED-1242 - Meeds-io/meeds#602 (#843)

--- portlets/src/main/webapp/vue-app/usersLeaderboard/components/UsersLeaderboardChart.vue
+++ portlets/src/main/webapp/vue-app/usersLeaderboard/components/UsersLeaderboardChart.vue
@@ -15,10 +15,11 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
-  <v-card flat class="pa-0">
-    <v-card-text class="pa-0">
-      <v-flex :id="id" class="usersLeaderboardChartParent" />
-    </v-card-text>
+  <v-card
+    :loading="loading"
+    class="pa-0"
+    flat>
+    <v-flex :id="id" class="usersLeaderboardChartParent" />
   </v-card>
 </template>
 <script>
@@ -49,77 +50,94 @@ export default {
   },
   data () {
     return {
+      id: `Chart${parseInt(Math.random() * 100000)}`,
+      loading: false,
       chartData: null,
-      id: `Chart${parseInt(Math.random() * 10000)
-        .toString()
-        .toString()}`,
+      chart: null,
+      echarts: null,
+      colors: [
+        '#4ad66d', '#ffe169', '#ff8fa3', '#20a8ea', '#C155F4', '#F7A35B', '#A0C7FF', '#FD6A6A', '#059d98', '#b7efc5',
+        '#dbb42c', '#c9184a', '#1273d4', '#E65ABC', '#00FF56', '#B1F6FF', '#FFFF46', '#26a855', '#f10000', '#208b3a',
+        '#c9a227', '#ffccd5', '#134d9b', '#E66CDC', '#58D68B', '#5CE6D3', '#f16a27', '#ac1c1e', '#eda3ff', '#1a7431',
+        '#a47e1b', '#ff4d6d', '#62b0de', '#FF97D0', '#92e03a', '#f44336', '#3d6d8a', '#E0A5FF', '#FF9DB8', '#808080'
+      ],
     };
-  },
-  computed: {
-    colors() {
-      return this.domains && this.domains.map(domain => domain.color) || [];
-    },
-    chartSettings() {
-      return this.chartData && {
-        tooltip: {
-          trigger: 'item',
-          formatter: '{b}: {c} ({d}%)'
-        },
-        color: this.chartData.map(data => data.color),
-        series: [{
-          type: 'pie',
-          radius: ['50%', '70%'],
-          data: this.chartData,
-        }]
-      };
-    }
   },
   watch: {
     open() {
-      this.init();
-    },
-    chartSettings() {
-      window.setTimeout(() => {
-        this.init();
-      }, 200);
+      if (this.open) {
+        this.refresh();
+      }
     },
   },
   mounted() {
-    const hexChars = '0123456';
-    this.domains.forEach(domain => {
-      if (!domain.color) {
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += hexChars[Math.floor(Math.random() * hexChars.length)];
-        }
-        domain.color = color;
-      }
-    });
-    this.loadChartData();
-    this.init();
+    this.refresh();
   },
   methods: {
+    refresh() {
+      this.loadChartData()
+        .then(() => this.init());
+    },
     loadChartData() {
-      fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/gamification/leaderboard/stats?username=${this.username}`, {
-        credentials: 'include',
-      })
-        .then(resp => resp && resp.ok && resp.json())
-        .then(chartData => {
-          chartData.forEach(statData => {
-            const domain = this.domains.find(tmpDomain => tmpDomain.title === statData.label);
-            statData.name = domain.label;
-            statData.color = domain.color;
-          });
-          this.chartData = chartData;
-        });
+      if (this.chartData) {
+        return this.$nextTick().then(() => this.chartData);
+      }
+
+      this.loading = true;
+      return new Promise((resolve, reject) => {
+        fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/gamification/leaderboard/stats?username=${this.username}`, {
+          credentials: 'include',
+        })
+          .then(resp => resp && resp.ok && resp.json())
+          .then(chartData => {
+            this.chartData = chartData;
+            chartData.forEach(statData => {
+              const domain = this.domains.find(tmpDomain => tmpDomain.title === statData.label);
+              statData.name = domain.label;
+            });
+            this.$nextTick().then(() => resolve(this.chartData));
+          })
+          .catch(e => reject(e))
+          .finally(() => this.loading = false);
+      });
     },
     init() {
-      if (!this.chartSettings) {
-        return;
-      }
-      window.require(['SHARED/eCharts'], echarts => {
-        const chart = echarts.init($(`#${this.id}`)[0]);
-        chart.setOption(this.chartSettings, true);
+      return new Promise((resolve, reject) => {
+        if (this.echarts) {
+          resolve(this.echarts);
+        } else {
+          try {
+            window.require(['SHARED/eCharts'], echarts => {
+              this.echarts = echarts;
+              resolve(echarts);
+            });
+          } catch (e) {
+            reject(e);
+          }
+        }
+      }).then(echarts => {
+        const element = document.querySelector(`#${this.id}`);
+        window.setTimeout(() => {
+          if (this.open) {
+            this.chart = echarts.init(element, null, {
+              width: 250,
+              height: 250,
+            });
+            this.chart.setOption({
+              tooltip: {
+                trigger: 'item',
+                formatter: '{b}: {c} ({d}%)'
+              },
+              color: this.colors,
+              series: [{
+                type: 'pie',
+                radius: ['50%', '70%'],
+                label: {show: false},
+                data: this.chartData,
+              }]
+            }, true);
+          }
+        }, 50);
       });
     }
   }
