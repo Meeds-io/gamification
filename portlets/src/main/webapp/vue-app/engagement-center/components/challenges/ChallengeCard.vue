2ecc8155959f4f129f9e1b054c7f76e9e200e38c
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-02-14 13:38:07 +0100

    fix: Days left are wrongly displayed for challenges - MEED-1776 (#896)
    Fix Feedback related to the numbers of left days in the challenge card.

--- portlets/src/main/webapp/vue-app/engagement-center/components/challenges/ChallengeCard.vue
+++ portlets/src/main/webapp/vue-app/engagement-center/components/challenges/ChallengeCard.vue
@@ -165,13 +165,13 @@ export default {
       return new Date(this.challenge?.endDate);
     },
     remainingPeriodLabel() {
-      if (this.endDate < new Date()) {
+      if (this.endDate.getTime() < new Date().getTime()) {
         return this.$t('challenges.label.over');
-      } else if (this.startDate > new Date()) {
+      } else if (this.startDate.getTime() > new Date().getTime()) {
         const days = Math.round((this.startDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)) + 1;
         return this.$t('challenges.label.openIn', {0: days});
       } else {
-        const days = Math.round((this.endDate.getTime() - this.startDate.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+        const days = Math.round((this.endDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)) + 1;
         return this.$t('challenges.label.daysLeft', {0: days});
       }
     },
