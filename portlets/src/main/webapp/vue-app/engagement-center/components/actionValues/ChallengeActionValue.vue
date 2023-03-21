5c60c90c97d83994453bccb65ba3144771d1b396
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-03-21 14:01:03 +0100

    fix: Fix Challenge Title truncation in Realizations - MEED-1952 - Meeds-io/meeds#811 (#953)

--- portlets/src/main/webapp/vue-app/engagement-center/components/actionValues/ChallengeActionValue.vue
+++ portlets/src/main/webapp/vue-app/engagement-center/components/actionValues/ChallengeActionValue.vue
@@ -17,16 +17,15 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 <template>
   <v-tooltip bottom>
     <template #activator="{ on }">
-      <div v-on="on">
-        <a
-          :class="!actionURL && 'not-clickable'"
-          class="flex-nowrap flex-grow-1 d-flex text-truncate container--fluid text-truncate"
-          :href="actionURL">
-          <v-icon size="15" class="primary--text">fas fa-trophy</v-icon>
-          <div class="ps-2 text-truncate">{{ actionLabel }}
-          </div>
-        </a>
-      </div>
+      <a
+        class="d-flex text-truncate"
+        :class="!actionURL && 'not-clickable'"
+        :href="actionURL"
+        v-on="on">
+        <v-icon size="15" class="primary--text">fas fa-trophy</v-icon>
+        <div class="ps-2 text-truncate">{{ actionLabel }}
+        </div>
+      </a>
     </template>
     <span>{{ actionLabel }}</span>
   </v-tooltip>
