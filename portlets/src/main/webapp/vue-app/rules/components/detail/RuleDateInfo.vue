a0a98647ce73f33320cda502d3595d636ca1f248
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-05-22 11:10:14 +0100

    feat: Add confeti animation on action announcement - MEED-1863 - Meeds-io/MIPs#49

---
+++ portlets/src/main/webapp/vue-app/rules/components/detail/RuleDateInfo.vue
@@ -0,0 +1,80 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <div>
+    {{ datesInfo }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    rule: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    startDateMillis() {
+      return this.rule?.startDate && new Date(this.rule?.startDate).getTime() || 0;
+    },
+    endDateMillis() {
+      return this.rule?.endDate && new Date(this.rule?.endDate).getTime() || 0;
+    },
+    notStartedYet() {
+      return this.startDateMillis && this.startDateMillis > Date.now();
+    },
+    alreadyEnded() {
+      return this.endDateMillis && this.endDateMillis < Date.now();
+    },
+    datesInfo() {
+      if (this.alreadyEnded) {
+        return this.$t('challenges.label.over');
+      } else if (this.notStartedYet) {
+        return this.$t('actions.label.opensIn', {0: this.getRemainingDateLabel(this.startDateMillis)});
+      } else if (this.endDateMillis) {
+        return this.$t('actions.label.endsIn', {0: this.getRemainingDateLabel(this.endDateMillis)});
+      }
+      return null;
+    },
+  },
+  watch: {
+    datesInfo: {
+      immediate: true,
+      handler() {
+        this.$emit('input', this.datesInfo);
+      },
+    },
+  },
+  methods: {
+    getRemainingDateLabel(timeInMs) {
+      const remainingSeconds = parseInt((timeInMs - Date.now()) / 1000);
+      const days = Math.floor(remainingSeconds / (60 * 60 * 24));
+      const hours = Math.floor((remainingSeconds % (60 * 60 * 24)) / (60 * 60));
+      if (days === 0) {
+        const minutes = Math.floor((remainingSeconds % (60 * 60)) / 60);
+        return this.$t('rules.card.timerShortHoursMinutes', {0: hours, 1: minutes});
+      } else {
+        return this.$t('rules.card.timerShortDaysHours', {0: days, 1: hours});
+      }
+    },
+  },
+};
+</script>
