455a749f8382bdf5996e4317fd6a59ab5aef94dc
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-05-24 18:38:54 +0100

    feat: Rework Engagement Center Actions Tab - MEED-2172 - MEED-1687 - Meeds-io/MIPs#49

---
+++ portlets/src/main/webapp/vue-app/engagement-center/components/rules/filter/RulesFilterDrawer.vue
@@ -0,0 +1,131 @@
+<!--
+
+  This file is part of the Meeds project (https://meeds.io/).
+
+  Copyright (C) 2023 Meeds Association contact@meeds.io
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <exo-drawer
+    ref="RealizationsFilterDrawer"
+    class="RealizationsFilterDrawer"
+    :right="!$vuetify.rtl">
+    <template #title>
+      {{ $t('gamification.actions.title.filterActions') }}
+    </template>
+    <template #content>
+      <v-card flat>
+        <v-card-title class="pb-0">
+          {{ $t('gamification.actions.filter.filterByType') }}
+        </v-card-title>
+        <v-card-text class="pb-0">
+          <v-radio-group v-model="type" class="ms-n1">
+            <v-radio
+              :label="$t('gamification.actions.filter.anyAction')"
+              value="ALL" />
+            <v-radio
+              :label="$t('gamification.actions.filter.manualAction')"
+              value="MANUAL" />
+            <v-radio
+              :label="$t('gamification.actions.filter.automaticAction')"
+              value="AUTOMATIC" />
+          </v-radio-group>
+        </v-card-text>
+        <v-card-title class="pb-0">
+          {{ $t('gamification.actions.filter.filterByStatus') }}
+        </v-card-title>
+        <v-card-text class="pb-0">
+          <v-radio-group v-model="status" class="ms-n1">
+            <v-radio
+              :label="$t('gamification.actions.filter.allActions')"
+              value="ALL" />
+            <v-radio
+              :label="$t('gamification.actions.filter.activeActions')"
+              value="STARTED" />
+            <v-radio
+              :label="$t('gamification.actions.filter.upcomingActions')"
+              value="NOT_STARTED" />
+            <v-radio
+              :label="$t('gamification.actions.filter.endedActions')"
+              value="ENDED" />
+            <v-radio
+              v-if="isAdministrator"
+              :label="$t('gamification.actions.filter.disabledActions')"
+              value="DISABLED" />
+          </v-radio-group>
+        </v-card-text>
+      </v-card>
+    </template>
+    <template #footer>
+      <div class="d-flex mr-2">
+        <v-btn
+          class="dark-grey-color px-0 hiddent-xs-only"
+          text
+          outlined
+          @click="reset">
+          <v-icon size="18" class="me-2">fa-redo</v-icon>
+          {{ $t('challenge.button.resetFilter') }}
+        </v-btn>
+        <v-spacer />
+        <v-btn
+          class="btn mx-1"
+          @click="close">
+          {{ $t('programs.details.filter.cancel') }}
+        </v-btn>
+        <v-btn
+          class="btn btn-primary"
+          @click="apply">
+          {{ $t('gamification.apply') }}
+        </v-btn>
+      </div>
+    </template>
+  </exo-drawer>
+</template>
+<script>
+export default {
+  props: {
+    isAdministrator: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      type: 'ALL',
+      status: 'STARTED',
+    };
+  },
+  created() {
+    this.$root.$on('rules-filter-drawer', this.open);
+    this.reset();
+  },
+  methods: {
+    open() {
+      this.$refs.RealizationsFilterDrawer.open();
+    },
+    close() {
+      this.$refs.RealizationsFilterDrawer.close();
+    },
+    reset() {
+      this.type = 'ALL';
+      this.status = 'STARTED';
+    },
+    apply() {
+      this.$emit('apply', this.type, this.status);
+      this.close();
+    },
+  }
+};
+</script>
--- portlets/src/main/webapp/vue-app/realizations/initComponents.js
+++ portlets/src/main/webapp/vue-app/realizations/initComponents.js
@@ -4,8 +4,8 @@ import RealizationItemMobile from './components/RealizationItemMobile.vue';
 import SelectPeriod from './components/SelectPeriod.vue';
 import NoResultFound from '../engagement-center/components/common/NoResultFound.vue';
 import ProgramSuggester from '../engagement-center/components/common/ProgramSuggester.vue';
-import FilterRealizationsDrawer from './components/FilterRealizationsDrawer.vue';
-import FilterRealizationsProgramList from './components/FilterRealizationsProgramList.vue';
+import RealizationFilterDrawer from './components/filter/RealizationFilterDrawer.vue';
+import RealizationProgramFilter from './components/filter/RealizationProgramFilter.vue';
 import GranteeAttendeeItem from './components/GranteeAttendeeItem.vue';
 import ProgramAttendeeItem from './components/ProgramAttendeeItem.vue';
 import RealizationExportButton from './components/RealizationExportButton.vue';
@@ -16,8 +16,8 @@ const components = {
   'realization-item': RealizationItem,
   'realization-item-mobile': RealizationItemMobile,
   'select-period': SelectPeriod,
-  'filter-realizations-drawer': FilterRealizationsDrawer,
-  'realizations-filter-program-list': FilterRealizationsProgramList,
+  'filter-realizations-drawer': RealizationFilterDrawer,
+  'realizations-filter-program-list': RealizationProgramFilter,
   'engagement-center-no-results': NoResultFound,
   'grantee-attendee-item': GranteeAttendeeItem,
   'program-attendee-item': ProgramAttendeeItem,
