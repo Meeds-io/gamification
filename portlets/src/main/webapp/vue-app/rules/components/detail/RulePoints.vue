5cf9af35662d3b28c002fa1d499a936c35385177
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-06 17:46:49 +0100

    feat: Enhance Rule Card UX - MEED-2226 - Meeds-io/MIPs#49 (#1068)

--- portlets/src/main/webapp/vue-app/rules/components/detail/RulePoints.vue
+++ portlets/src/main/webapp/vue-app/rules/components/detail/RulePoints.vue
@@ -23,7 +23,7 @@
     <div class="text-sub-title font-italic mb-1">
       {{ $t('rules.achieveItFor') }}
     </div>
-    <v-chip class="secondary secondary-border-color content-box-sizing white--text" small>
+    <v-chip class="error-color-background opacity-8 content-box-sizing white--text" small>
       <span class="subtitle-2">+ {{ rule.score }}</span>
     </v-chip>
   </div>
