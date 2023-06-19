4f20b64c6b9b88e9d0cc01907fb272f19b7d1256
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-19 18:09:24 +0100

    feat: Transform Announcement publication to Activity comment - MEED-2244 - Meeds-io/MIPs#50 (#1095)

--- portlets/src/main/webapp/vue-app/engagement-center-activity-stream-extension/components/RuleActivity.vue
+++ portlets/src/main/webapp/vue-app/engagement-center-activity-stream-extension/components/RuleActivity.vue
@@ -154,15 +154,23 @@ export default {
   created() {
     this.$root.$on('rule-updated', this.updateRule);
     this.$root.$on('rule-deleted', this.deleteRule);
+    this.$root.$on('announcement-added', this.refreshComments);
   },
   beforeDestroy() {
     this.$root.$off('rule-updated', this.updateRule);
     this.$root.$off('rule-deleted', this.deleteRule);
+    this.$root.$off('announcement-added', this.refreshComments);
   },
   methods: {
     openRule() {
       if (this.rule?.userInfo) {
         this.$root.$emit('rule-detail-drawer-by-id', this.ruleId);
+      }
+    },
+    refreshComments(event) {
+      if (this.ruleId === event?.detail?.challengeId) {
+        this.$activityService.getActivityById(`comment${event.detail.announcement.activityId}`)
+          .then(comment => this.$root.$emit('activity-comment-created', comment));
       }
     },
     updateRule(rule) {
