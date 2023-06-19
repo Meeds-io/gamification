b56fbcf0c9007f75a5d01a945d63b203e7be0194
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-19 09:40:58 +0100

    fix: Add missing Participants components to display on Activity - MEED-2240 (#1091)

--- portlets/src/main/webapp/vue-app/engagement-center-activity-stream-extension/initComponents.js
+++ portlets/src/main/webapp/vue-app/engagement-center-activity-stream-extension/initComponents.js
@@ -19,11 +19,13 @@
 import ActivityAnnouncement from './components/ActivityAnnouncement.vue';
 import RuleActivity from './components/RuleActivity.vue';
 import RuleActivityAnnounceAction from './components/RuleActivityAnnounceAction.vue';
+import AvatarsList from '../engagement-center/components/common/AvatarsList.vue';
 
 const components = {
   'activity-announcement': ActivityAnnouncement,
   'rule-activity': RuleActivity,
   'rule-activity-announce-action': RuleActivityAnnounceAction,
+  'engagement-center-avatars-list': AvatarsList,
 };
 
 for (const key in components) {
