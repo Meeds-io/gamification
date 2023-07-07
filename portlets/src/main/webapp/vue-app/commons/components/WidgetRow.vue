a33702925b02f3529f906e78c7c0181d03ca37d1
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-07-07 16:58:08 +0100

    feat: Add Program Drawer used in Overview Page - MEED-2078 - Meeds-io/MIPs#50 (#1129)
    This change will introduce a new UX/UI in Overvie Page to access a
    program summary from a drawer.

--- portlets/src/main/webapp/vue-app/commons/components/WidgetRow.vue
+++ portlets/src/main/webapp/vue-app/commons/components/WidgetRow.vue
@@ -4,10 +4,9 @@
       <slot name="title"></slot>
     </div>
     <v-list-item
-      v-on="isChallengeIdProvided ? { click: clickEvent } : {}"
-      :href="redirectionUrl"
+      v-on="clickable ? { click: open } : {}"
       :dense="!normalHeight"
-      class="px-0 no-ripple">
+      class="px-0">
       <v-list-item-action-text v-if="$slots.icon" class="me-4">
         <slot name="icon"></slot>
       </v-list-item-action-text>
@@ -30,23 +29,19 @@ export default {
       type: Boolean,
       default: false,
     },
-    redirectionUrl: {
-      type: String,
-      default: () => '',
-    },
     normalHeight: {
       type: Boolean,
       default: false,
     },
-    clickEventParam: {
-      type: String,
-      default: () => '',
+    clickable: {
+      type: Boolean,
+      default: false,
     },
   },
   methods: {
-    clickEvent() {
-      if (this.isChallengeIdProvided) {
-        document.dispatchEvent(new CustomEvent('widget-row-click-event', {detail: this.clickEventParam}));
+    open() {
+      if (!this.redirectionUrl) {
+        this.$emit('open');
       }
     }
   },
