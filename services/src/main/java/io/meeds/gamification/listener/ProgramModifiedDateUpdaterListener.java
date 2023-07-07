dbc4b403f0fc6fb984a8473843ba4dc85abfffb4
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-07-07 13:42:53 +0100

    feat: Review Programs List Overview Widget - MEED-2314 - Meeds-io/MIPs#50 (#1128)
    This change will enhance UX & UI of programs listed in overview page.

---
+++ services/src/main/java/io/meeds/gamification/listener/ProgramModifiedDateUpdaterListener.java
@@ -0,0 +1,48 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.gamification.listener;
+
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+
+import io.meeds.gamification.model.RuleDTO;
+import io.meeds.gamification.service.ProgramService;
+import io.meeds.gamification.service.RuleService;
+
+public class ProgramModifiedDateUpdaterListener extends Listener<Long, String> {
+
+  private ProgramService programService;
+
+  private RuleService    ruleService;
+
+  public ProgramModifiedDateUpdaterListener(ProgramService programService,
+                                            RuleService ruleService) {
+    this.programService = programService;
+    this.ruleService = ruleService;
+  }
+
+  @Override
+  public void onEvent(Event<Long, String> event) throws Exception {
+    long ruleId = event.getSource();
+    RuleDTO rule = ruleService.findRuleById(ruleId);
+    if (rule != null) {
+      programService.updateProgramDate(rule.getProgramId());
+    }
+  }
+
+}
