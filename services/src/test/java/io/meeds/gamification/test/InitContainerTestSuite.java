0b39845f9d0746147588e78bf4793b48cd051a1e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-07-11 11:41:40 +0100

    feat: Trigger Activity Pin Gamification Event - MEED-2207 - Meeds-io/MIPs#50 (#1137)
    This change will allow to gamify 'pin activity' space host action.

--- services/src/test/java/io/meeds/gamification/test/InitContainerTestSuite.java
+++ services/src/test/java/io/meeds/gamification/test/InitContainerTestSuite.java
@@ -34,6 +34,7 @@ import io.meeds.gamification.dao.ProgramDAOTest;
 import io.meeds.gamification.dao.RealizationDAOTest;
 import io.meeds.gamification.dao.RuleDAOTest;
 import io.meeds.gamification.listener.AnnouncementActivityUpdaterTest;
+import io.meeds.gamification.listener.GamificationActivityListenerTest;
 import io.meeds.gamification.listener.GamificationNotificationListenerTest;
 import io.meeds.gamification.listener.GamificationProfileListenerTest;
 import io.meeds.gamification.listener.GamificationRelationshipListenerTest;
@@ -112,6 +113,7 @@ import io.meeds.gamification.utils.UtilsTest;
     AnalyticsProgramListenerTest.class,
     AnalyticsRuleListenerTest.class,
     GamificationSpaceListenerTest.class,
+    GamificationActivityListenerTest.class,
     ProgramSpaceListenerTest.class,
     ProgramAutoDisableListenerTest.class,
     ProgramAutoDisableJobTest.class,
