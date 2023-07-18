b37811f2721ffc5427a9df0c560cbdbb53f03c1e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-07-18 07:10:35 +0100

    fix: Fix Rule Access link Menu Item - MEED-2349 - MEED-2350 - Meeds-io/MIPs#50 Prior to this change, the Rule Drawer Menu displays 'Open Action' menu item when in programs application while it should be 'Open Activity'. In addition, the Announcement form wasn't all time displayed in Activity stream right after clicking on 'Announce' button from activity footer. This change will fix and enhance both behaviors.

--- portlets/src/main/webapp/vue-app/rules-activity-stream-extension/initComponents.js
+++
@@ -1,8 +0,0 @@
-import RuleActivity from './components/favorites/RuleActivity.vue';
-const components = {
-  'rule-activity': RuleActivity,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
