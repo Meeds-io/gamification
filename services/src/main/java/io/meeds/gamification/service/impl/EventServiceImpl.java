fce8db09cabd996c0959db7ca4e150b7fa77050d
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-09-08 15:26:09 +0200

    feat: Gamification connector List cancelled case - MEED-2441 - Meeds-io/MIPs#64 (#1194)
    Update events config

--- services/src/main/java/io/meeds/gamification/service/impl/EventServiceImpl.java
+++ services/src/main/java/io/meeds/gamification/service/impl/EventServiceImpl.java
@@ -21,16 +21,12 @@ import io.meeds.gamification.model.EventDTO;
 import io.meeds.gamification.model.filter.EventFilter;
 import io.meeds.gamification.service.EventService;
 import io.meeds.gamification.storage.EventStorage;
-import io.meeds.gamification.utils.Utils;
-import org.apache.commons.lang3.StringUtils;
 import org.exoplatform.commons.ObjectAlreadyExistsException;
 import org.exoplatform.commons.exception.ObjectNotFoundException;
 
 import java.util.*;
 
 public class EventServiceImpl implements EventService {
-
-  public static final String ENABLED = ".enabled";
 
   private final EventStorage eventStorage;
 
@@ -84,33 +80,5 @@ public class EventServiceImpl implements EventService {
 
   public EventDTO getEvent(long eventId) {
     return eventStorage.getEventById(eventId);
-  }
-
-  @Override
-  public void setEventEnabledForProject(long eventId,
-                                        long projectId,
-                                        boolean enabled,
-                                        String currentUser) throws IllegalAccessException, ObjectNotFoundException {
-    if (!Utils.isRewardingManager(currentUser)) {
-      throw new IllegalAccessException("The user is not authorized to enable/disable event");
-    }
-    EventDTO eventDTO = eventStorage.getEventById(eventId);
-    if (eventDTO == null) {
-      throw new ObjectNotFoundException("event not found");
-    }
-    Map<String, String> eventProperties = eventDTO.getProperties();
-    if (eventProperties != null && !eventProperties.isEmpty()) {
-      String eventProjectStatus = eventProperties.get(projectId + ENABLED);
-      if (StringUtils.isNotBlank(eventProjectStatus)) {
-        eventProperties.remove(projectId + ENABLED);
-        eventProperties.put(projectId + ENABLED, String.valueOf(enabled));
-        eventDTO.setProperties(eventProperties);
-      }
-    } else {
-      Map<String, String> properties = new HashMap<>();
-      properties.put(projectId + ENABLED, String.valueOf(enabled));
-      eventDTO.setProperties(properties);
-    }
-    eventStorage.updateEvent(eventDTO);
   }
 }
