9277bc4f726e113f3648002d0c299c66f234c9c5
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-13 15:05:21 +0100

    feat: Enhance Web Notification UI - MEED-2483 - Meeds-io/MIPs#80 (#1200)

---
+++ portlets/src/main/webapp/vue-app/notification-extension/components/RuleAnnounced.vue
@@ -0,0 +1,85 @@
+<template>
+  <user-notification-template
+    :loading="loading"
+    :notification="notification"
+    :avatar-url="profileAvatarUrl"
+    :message="message"
+    :url="activityUrl">
+    <template #actions>
+      <div class="text-truncate">
+        <rule-icon :rule-event="ruleEvent" size="14" />
+        {{ ruleTitle }}
+      </div>
+      <div class="my-1">
+        <v-btn
+          :href="replyUrl"
+          color="primary"
+          elevation="0"
+          small
+          outlined>
+          <v-icon size="14" class="me-1">fa-handshake</v-icon>
+          <span class="text-none">
+            {{ $t('gamification.label.ruleAnnouncementButton') }}
+          </span>
+        </v-btn>
+      </div>
+    </template>
+  </user-notification-template>
+</template>
+<script>
+export default {
+  props: {
+    notification: {
+      type: Object,
+      default: null,
+    },
+  },
+  data: () => ({
+    loading: true,
+    announcement: null,
+  }),
+  computed: {
+    announcementId() {
+      return this.notification?.parameters?.ANNOUNCEMENT_ID;
+    },
+    rule() {
+      return this.announcement?.action;
+    },
+    ruleTitle() {
+      return this.rule?.title;
+    },
+    ruleEvent() {
+      return this.rule?.event;
+    },
+    activityId() {
+      return this.notification?.parameters?.activityId;
+    },
+    activityUrl() {
+      return this.activityId && `${eXo.env.portal.context}/${eXo.env.portal.portalName}/activity?id=${this.activityId}`;
+    },
+    replyUrl() {
+      return this.activityId && `${eXo.env.portal.context}/${eXo.env.portal.portalName}/activity?id=${this.activityId}#comment-reply`
+        || '#';
+    },
+    profile() {
+      return this.announcement?.earner?.profile;
+    },
+    profileAvatarUrl() {
+      return this.profile?.avatar;
+    },
+    message() {
+      return this.profile && this.$t('Notification.gamification.webNotif.newAchievement', {
+        0: `<a class="user-name font-weight-bold">${this.profile.fullname}</a>`,
+      }) || '';
+    },
+  },
+  created() {
+    this.$realizationService.getRealizationById(this.announcementId)
+      .then(announcement => {
+        this.announcement = announcement;
+        return this.$nextTick();
+      })
+      .finally(() => this.loading = false);
+  },
+};
+</script>
--- portlets/src/main/webapp/vue-app/notification-extension/initComponents.js
+++ portlets/src/main/webapp/vue-app/notification-extension/initComponents.js
@@ -1,7 +1,7 @@
-/**
+/*
  * This file is part of the Meeds project (https://meeds.io/).
  *
- * Copyright (C) 2023 Meeds Association contact@meeds.io
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -9,13 +9,22 @@
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-export function init() {
-  Vue.prototype.$utils?.includeExtensions('engagementCenterActions');
+import RulePublished from './components/RulePublished.vue';
+import RuleAnnounced from './components/RuleAnnounced.vue';
+
+const components = {
+  'user-notification-rule-published': RulePublished,
+  'user-notification-rule-announced': RuleAnnounced,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
 }
