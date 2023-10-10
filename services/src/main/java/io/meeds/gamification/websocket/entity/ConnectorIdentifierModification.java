c4f59a6cd2c23dd5f18de4e06237bd2719cb5fb3
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-10-10 17:24:48 +0200

    feat: Clean Twitter static connector - MEED-2567 - MEED-2620 - Meeds-io/MIPs#94 (#1245)

--- services/src/main/java/io/meeds/gamification/websocket/entity/ConnectorIdentifierModification.java
+++ services/src/main/java/io/meeds/gamification/websocket/entity/ConnectorIdentifierModification.java
@@ -2,44 +2,46 @@
  * This file is part of the Meeds project (https://meeds.io/).
  *
  * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
- *
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package io.meeds.gamification.model;
+package io.meeds.gamification.websocket.entity;
 
-import java.io.Serializable;
+import org.exoplatform.ws.frameworks.json.impl.JsonException;
+import org.exoplatform.ws.frameworks.json.impl.JsonGeneratorImpl;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
-public class RemoteConnector implements Serializable {
+@AllArgsConstructor
+public class ConnectorIdentifierModification {
 
-  private static final long serialVersionUID = -5423886714771209536L;
+  private String eventName;
 
-  protected String  name;
+  private String connectorName;
 
-  protected String  apiKey;
+  private String username;
 
-  protected String  redirectUrl;
-
-  protected boolean enabled;
+  private String connectorIdentifier;
 
   @Override
-  public RemoteConnector clone() { // NOSONAR
-    return new RemoteConnector(name, apiKey, redirectUrl, enabled);
+  public String toString() {
+    try {
+      return new JsonGeneratorImpl().createJsonObject(this).toString();
+    } catch (JsonException e) {
+      throw new IllegalStateException("Error parsing object to JSON string", e);
+    }
   }
 }
