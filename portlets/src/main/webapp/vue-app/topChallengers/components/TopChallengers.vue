49581ab336a1c06a82149900ccb7b939cb7bac7b
saraboutej <sboutej@exoplatform.com>
2023-10-13 16:50:20 +0100

    feat: Apply generic UI to existing portlet - MEED-2653 - Meeds-io/MIPs#81
    (cherry picked from commit ffe643fc829c6d774127480f2a6d230477544fdb)

--- portlets/src/main/webapp/vue-app/topChallengers/components/TopChallengers.vue
+++ portlets/src/main/webapp/vue-app/topChallengers/components/TopChallengers.vue
@@ -21,7 +21,8 @@
 -->
 <template>
   <v-app>
-    <gamification-overview-widget :loading="loading">
+    <gamification-overview-widget
+      :loading="loading">
       <template #title>
         <div v-if="!displayPlaceholder && !loading" class="d-flex flex-grow-1 full-width">
           <div class="widget-text-header text-capitalize-first-letter text-truncate">
@@ -38,27 +39,25 @@
           </v-btn>
         </div>
       </template>
-      <template #content>
-        <gamification-overview-widget-row
-          v-if="displayPlaceholder"
-          disabled
-          class="my-auto">
-          <template #content>
-            <div class="d-flex flex-column align-center justify-center">
-              <v-icon color="secondary" size="54">fa-trophy</v-icon>
-              <span class="subtitle-1 font-weight-bold mt-7">{{ $t('gamification.overview.weeklyLeaderboard') }}</span>
-            </div>
-          </template>
-        </gamification-overview-widget-row>
-        <gamification-overview-widget-row
-          v-show="!displayPlaceholder"
-          class="my-auto"
-          dense>
-          <template #content>
-            <gamification-rank :is-overview-display="true" />
-          </template>
-        </gamification-overview-widget-row>
-      </template>
+      <gamification-overview-widget-row
+        v-if="displayPlaceholder"
+        disabled
+        class="my-auto">
+        <template #content>
+          <div class="d-flex flex-column align-center justify-center">
+            <v-icon color="secondary" size="54">fa-trophy</v-icon>
+            <span class="subtitle-1 font-weight-bold mt-7">{{ $t('gamification.overview.weeklyLeaderboard') }}</span>
+          </div>
+        </template>
+      </gamification-overview-widget-row>
+      <gamification-overview-widget-row
+        v-show="!displayPlaceholder"
+        class="my-auto"
+        dense>
+        <template #content>
+          <gamification-rank :is-overview-display="true" />
+        </template>
+      </gamification-overview-widget-row>
     </gamification-overview-widget>
     <gamification-overview-leaderboard-drawer
       ref="detailsDrawer" />
