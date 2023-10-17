b126274878713768e1791b7cfc8bebe6fdd4cab9
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-10-17 18:20:38 +0200

    feat: Gamification Connector Oauth Settings - MEED-2567 - MEED-2620 - Meeds-io/MIPs#94 (#1249)
    Fix feedbacks

--- portlets/src/main/webapp/vue-app/connector-user-settings/js/GamificationConnectorService.js
+++ portlets/src/main/webapp/vue-app/connector-user-settings/js/GamificationConnectorService.js
@@ -1,7 +1,8 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
- * Copyright (C) 2020 - 2023 Meeds Association
- * contact@meeds.io
+ *
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
