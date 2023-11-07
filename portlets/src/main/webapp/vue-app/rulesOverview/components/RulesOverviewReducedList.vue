19dd41929096bc905534a9085c111d940722e2a9
Boubaker Khanfir <bkhanfir@exoplatform.com>
2023-11-07 17:14:00 +0100

    feat: Rework Overview Widgets Placeholders - MEED-2860 - Meeds-io/MIPs#104 (#1325)
    This change will simplify Overview widget placeholders.

--- portlets/src/main/webapp/vue-app/rulesOverview/components/RulesOverviewReducedList.vue
+++ portlets/src/main/webapp/vue-app/rulesOverview/components/RulesOverviewReducedList.vue
@@ -29,7 +29,7 @@
           min-width="auto"
           class="pa-0"
           text
-          @click="$refs.listDrawer.open()">
+          @click="$emit('open-list')">
           <span class="primary--text text-none">{{ $t('rules.seeAll') }}</span>
         </v-btn>
       </div>
@@ -77,20 +77,11 @@
           :key="index"
           class="flex" />
       </template>
-      <gamification-rules-overview-list-drawer
-        ref="listDrawer" />
     </template>
-    <gamification-overview-widget-row
-      v-else
-      v-show="!loading"
-      class="my-auto">
-      <template #content>
-        <div class="d-flex flex-column align-center justify-center">
-          <v-icon color="secondary" size="48">fa-rocket</v-icon>
-          <span class="subtitle-1 font-weight-bold mt-7">{{ $t('gamification.overview.actions') }}</span>
-        </div>
-      </template>
-    </gamification-overview-widget-row>
+    <div v-else-if="!loading" class="d-flex flex-column align-center justify-center full-width full-height">
+      <v-icon color="secondary" size="48">fa-rocket</v-icon>
+      <span class="subtitle-1 font-weight-bold mt-7">{{ $t('gamification.overview.actions') }}</span>
+    </div>
   </gamification-overview-widget>
 </template>
 <script>
