882a2cf40c654b5e9f97b43c4fc65d3aba2590e4
Azmi Touil <atouil@exoplatform.com>
2023-12-07 13:45:26 +0100

    feat: Implement new common connector extension - MEED-2962 - Meeds-io/MIPs#105

--- portlets/src/main/webapp/vue-app/connector-admin-settings/components/AdminConnectorDetail.vue
+++ portlets/src/main/webapp/vue-app/connector-admin-settings/components/AdminConnectorDetail.vue
@@ -78,9 +78,9 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
           </v-card>
         </v-subheader>
         <gamification-admin-connector-trigger
-          v-for="event in triggersToDisplay"
-          :key="event.title"
-          :trigger="event"
+          v-for="trigger in triggersToDisplay"
+          :key="trigger.title"
+          :trigger="trigger"
           class="py-2" />
       </v-list>
     </v-card>
@@ -116,13 +116,13 @@ export default {
   },
   computed: {
     title() {
-      return this.connectorExtension?.componentOptions?.title || '';
+      return this.connectorExtension?.title || '';
     },
     name() {
-      return this.connectorExtension?.componentOptions?.name || '';
+      return this.connectorExtension?.name || '';
     },
     description() {
-      return this.$t(`${this.connectorExtension?.componentOptions?.description}`);
+      return this.$t(`${this.connectorExtension?.description}`);
     },
     connectorActivated() {
       return this.connector?.apiKey && this.connector?.secretKey && this.connector?.redirectUrl && this.connector?.enabled;
@@ -152,7 +152,7 @@ export default {
       return this.sortedTriggers?.slice(0, this.pageSize);
     },
     idDefaultConnector() {
-      return this.connectorExtension?.componentOptions?.defaultConnector;
+      return this.connectorExtension?.defaultConnector;
     },
   },
   methods: {
@@ -160,7 +160,7 @@ export default {
       this.$root.$emit('close-connector-detail');
     },
     openConnectorSettings() {
-      this.$root.$emit('open-connector-settings', this.connector, this.connectorExtension);
+      this.$root.$emit('open-connector-settings', this.connector);
     },
     loadMore() {
       this.pageSize += this.pageSize;
--- portlets/src/main/webapp/vue-app/connectors-extensions/services.js
+++ portlets/src/main/webapp/vue-app/connectors-extensions/services.js
@@ -1,4 +1,5 @@
 /*
+ *
  * This file is part of the Meeds project (https://meeds.io/).
  *
  * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
@@ -11,32 +12,16 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
-import './initComponents.js';
 
-const appId = 'connectorAdminSettings';
+import * as gamificationConnectorService from './js/GamificationConnectorService';
 
-//getting language of the PLF
-const lang = eXo.env.portal.language || 'en';
-
-//should expose the locale ressources as REST API 
-const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.Challenges-${lang}.json`;
-
-export function init() {
-  exoi18n.loadLanguageAsync(lang, url)
-    .then(i18n => {
-      Vue.createApp({
-        template: `<gamification-admin-connector-settings id="${appId}" />`,
-        i18n,
-        vuetify: Vue.prototype.vuetifyOptions,
-      }, `#${appId}`, 'Admin Connectors Settings App');
-    });
+if (!Vue.prototype.$gamificationConnectorService) {
+  window.Object.defineProperty(Vue.prototype, '$gamificationConnectorService', {
+    value: gamificationConnectorService,
+  });
 }
-Vue.prototype.$utils.includeExtensions('gamificationAdminConnectorsExtensions');
-Vue.prototype.$utils.includeExtensions('engagementCenterConnectors');
-
-
