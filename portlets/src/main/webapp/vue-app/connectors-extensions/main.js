6ea5c480b475e72774704befd03e7baf57c26868
Azmi Touil <atouil@exoplatform.com>
2023-12-07 13:45:26 +0100

    feat: Implement new common connector extension - MEED-2962 - Meeds-io/MIPs#105

--- portlets/src/main/webapp/vue-app/connectors-extensions/main.js
+++ portlets/src/main/webapp/vue-app/connectors-extensions/main.js
@@ -1,5 +1,4 @@
 /*
- *
  * This file is part of the Meeds project (https://meeds.io/).
  *
  * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
@@ -12,16 +11,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
  */
+import './services.js';
+import './extensions.js';
 
-import * as gamificationConnectorService from '../connector-user-settings/js/GamificationConnectorService';
+Vue.prototype.$utils?.includeExtensions('engagementCenterConnectors');
 
-if (!Vue.prototype.$gamificationConnectorService) {
-  window.Object.defineProperty(Vue.prototype, '$gamificationConnectorService', {
-    value: gamificationConnectorService,
-  });
-}
