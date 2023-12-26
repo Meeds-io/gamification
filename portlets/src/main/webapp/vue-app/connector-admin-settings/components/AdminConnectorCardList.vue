3a4883d91657862dfbe6274b4b153d510c0653f6
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-12-26 16:20:15 +0100

    fix: Update the color (dark grey) in connectors admin - MEED-3067 - Meeds-io/meeds#1430 (#1403)

--- portlets/src/main/webapp/vue-app/connector-admin-settings/components/AdminConnectorCardList.vue
+++ portlets/src/main/webapp/vue-app/connector-admin-settings/components/AdminConnectorCardList.vue
@@ -16,7 +16,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
   <v-card v-if="enabledConnectors && enabledConnectors.length" flat>
-    <div class="dark-grey-color font-weight-bold icon-default-size py-2 py-sm-5 d-flex align-center">{{ $t('gamification.connectors.label.connectors') }}</div>
+    <div class="text-color font-weight-bold icon-default-size py-2 py-sm-5 d-flex align-center">{{ $t('gamification.connectors.label.connectors') }}</div>
     <v-card-text class="text-font-size pb-2 pt-0 pb-sm-5 px-0 d-flex align-center">{{ $t('gamification.connectors.label.valueContributions') }}</v-card-text>
     <v-card-text class="pa-0">
       <v-item-group>
@@ -34,7 +34,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
         </v-container>
       </v-item-group>
     </v-card-text>
-    <div class="font-weight-bold dark-grey-color text-subtitle-1 py-2 py-sm-5 d-flex align-center">{{ $t('gamification.connectors.label.comingSoon') }}</div>
+    <div class="text-color font-weight-bold text-subtitle-1 py-2 py-sm-5 d-flex align-center">{{ $t('gamification.connectors.label.comingSoon') }}</div>
     <v-card-text class="pa-0">
       <v-item-group>
         <v-container class="pa-0">
