63896a551382c8b06561b2abe2a234b7d7922b2d
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-02-09 12:36:28 +0100

    feat: Add remind where or what to do the action for meeds events - MEED-3284 - Meeds-io/MIPs#105 (#1467)
    This PR will add information in the action detail drawer so it can be
    useful to remind where or what to do the action

---
+++ portlets/src/main/webapp/vue-app/connector-events-extensions/components/profile/ProfileEvent.vue
@@ -0,0 +1,68 @@
+<!--
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-app v-if="!isEditing">
+    <div class="subtitle-1 font-weight-bold mb-2">
+      {{ $t('gamification.event.display.goThere') }}
+    </div>
+    <div class="d-flex justify-center">
+      <v-btn
+        :href="buttonLink"
+        max-width="250"
+        class="ignore-vuetify-classes text-capitalize btn btn-primary">
+        {{ buttonLabel }}
+      </v-btn>
+    </div>
+  </v-app>
+</template>
+<script>
+export default {
+  props: {
+    properties: {
+      type: Object,
+      default: null
+    },
+    trigger: {
+      type: String,
+      default: null
+    },
+    isEditing: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    isProfileEvent() {
+      return !['receiveRelationshipRequest', 'sendRelationshipRequest'].includes(this.trigger);
+    },
+    profileLink() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/profile/${eXo.env.portal.userName}`;
+    },
+    peopleLink() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/people`;
+    },
+    buttonLabel() {
+      return this.isProfileEvent ? this.$t('gamification.event.display.yourProfile') : this.$t('gamification.event.display.connectWithOthers');
+    },
+    buttonLink() {
+      return this.isProfileEvent ? this.profileLink : this.peopleLink;
+    }
+  }
+};
+</script>
