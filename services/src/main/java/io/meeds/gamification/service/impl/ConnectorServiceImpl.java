66ecae49338f9ab51ccf3284f84fd8c86b5d9701
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2024-03-01 09:27:47 +0100

    feat: Add Evm connector plugin - MEED-3205 - Meeds-io/MIPs#118 (#1496)
    This change will Add the evm connector application depending on the activation of the feature flag.

--- services/src/main/java/io/meeds/gamification/service/impl/ConnectorServiceImpl.java
+++ services/src/main/java/io/meeds/gamification/service/impl/ConnectorServiceImpl.java
@@ -27,6 +27,7 @@ import io.meeds.gamification.websocket.entity.ConnectorIdentifierModification;
 import org.apache.commons.lang3.StringUtils;
 
 import org.exoplatform.commons.ObjectAlreadyExistsException;
+import org.exoplatform.commons.api.settings.ExoFeatureService;
 import org.exoplatform.services.listener.ListenerService;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
@@ -66,14 +67,18 @@ public class ConnectorServiceImpl implements ConnectorService {
 
   private final ListenerService              listenerService;
 
+  private ExoFeatureService                  featureService;
+
   public ConnectorServiceImpl(ConnectorAccountStorage connectorAccountStorage,
                               IdentityManager identityManager,
                               ConnectorSettingService connectorSettingService,
-                              ListenerService listenerService) {
+                              ListenerService listenerService,
+                              ExoFeatureService featureService) {
     this.connectorAccountStorage = connectorAccountStorage;
     this.connectorSettingService = connectorSettingService;
     this.identityManager = identityManager;
     this.listenerService = listenerService;
+    this.featureService = featureService;
   }
 
   @Override
