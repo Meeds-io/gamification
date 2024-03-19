1a2d28e83687b8e39ca829175d0a853ac36f1336
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-03-19 15:13:54 +0100

    feat: Cancelled contribution cannot be accepted/rejected - MEED-4350 - Meeds-io/MIPs#122 (#1510)
    This PR will ensure that it will not be possible to update the canceled
    achievement

--- services/src/main/java/io/meeds/gamification/service/impl/RealizationServiceImpl.java
+++ services/src/main/java/io/meeds/gamification/service/impl/RealizationServiceImpl.java
@@ -299,6 +299,9 @@ public class RealizationServiceImpl implements RealizationService, Startable {
       throw new IllegalAccessException("User doesn't have enough privileges to update achievements of user"
           + realization.getEarnerId());
     }
+    if (RealizationStatus.CANCELED.name().equals(realization.getStatus())) {
+      throw new IllegalArgumentException("Canceled achievement cannot be updated");
+    }
     updateRealizationStatus(realization, status);
   }
 
