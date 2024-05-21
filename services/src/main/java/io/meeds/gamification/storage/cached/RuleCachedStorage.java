33f64894d54fc2b2c1ffb7ed388cd2ca9e05d2f6
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-05-21 18:29:00 +0100

    fix: Improve Gamification Rules and Realization Performances - MEED-6827 - Meeds-io/meeds#1989 (#1588)
    This change will improve performances of rules and realizations by
    introducing cache management.

--- services/src/main/java/io/meeds/gamification/storage/cached/RuleCachedStorage.java
+++ services/src/main/java/io/meeds/gamification/storage/cached/RuleCachedStorage.java
@@ -18,31 +18,42 @@
 package io.meeds.gamification.storage.cached;
 
 import java.io.Serializable;
+import java.util.List;
 
-import io.meeds.gamification.storage.EventStorage;
 import org.exoplatform.commons.cache.future.FutureExoCache;
 import org.exoplatform.commons.cache.future.Loader;
 import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.services.cache.CacheService;
+import org.exoplatform.services.cache.CachedObjectSelector;
 import org.exoplatform.services.cache.ExoCache;
+import org.exoplatform.services.cache.ObjectCacheInfo;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 
-import io.meeds.gamification.constant.EntityType;
 import io.meeds.gamification.dao.ProgramDAO;
 import io.meeds.gamification.dao.RuleDAO;
 import io.meeds.gamification.model.RuleDTO;
+import io.meeds.gamification.model.filter.RuleFilter;
+import io.meeds.gamification.storage.EventStorage;
 import io.meeds.gamification.storage.ProgramStorage;
 import io.meeds.gamification.storage.RuleStorage;
 import io.meeds.gamification.storage.cached.model.CacheKey;
 
 public class RuleCachedStorage extends RuleStorage {
 
-  private static final int                               RULE_ID_CONTEXT         = 0;
+  public static final String                             RULE_CACHE_NAME               = "gamification.rule";
 
-  private static final int                               RULE_TITLE_CONTEXT      = 1;
+  public static final int                                RULE_ID_CONTEXT               = 0;
 
-  private static final int                               RULES_BY_FILTER_CONTEXT = 3;
+  public static final int                                RULE_TITLE_CONTEXT            = 1;
 
-  private static final String                            RULE_CACHE_NAME         = "gamification.rule";
+  public static final int                                RULES_BY_FILTER_CONTEXT       = 3;
+
+  public static final int                                RULES_COUNT_BY_FILTER_CONTEXT = 4;
+
+  private static final Log                               LOG                           = ExoLogger.getLogger(RuleCachedStorage.class);
+
+  private ExoCache<Serializable, Object>                 ruleCache;
 
   private FutureExoCache<Serializable, Object, CacheKey> ruleFutureCache;
 
@@ -52,8 +63,8 @@ public class RuleCachedStorage extends RuleStorage {
                            RuleDAO ruleDAO,
                            CacheService cacheService) {
     super(programStorage, eventStorage, programDAO, ruleDAO);
-    ExoCache<Serializable, Object> ruleCache = cacheService.getCacheInstance(RULE_CACHE_NAME);
-    Loader<Serializable, Object, CacheKey> ruleLoader = new Loader<Serializable, Object, CacheKey>() {
+    this.ruleCache = cacheService.getCacheInstance(RULE_CACHE_NAME);
+    this.ruleFutureCache = new FutureExoCache<>(new Loader<Serializable, Object, CacheKey>() {
       @Override
       public Object retrieve(CacheKey context, Serializable key) throws Exception {
         if (context.getContext() == RULE_ID_CONTEXT) {
@@ -66,25 +77,37 @@ public class RuleCachedStorage extends RuleStorage {
           return RuleCachedStorage.super.findRuleByTitle(context.getTitle());
         } else if (context.getContext() == RULES_BY_FILTER_CONTEXT) {
           return RuleCachedStorage.super.findRuleIdsByFilter(context.getRuleFilter(), context.getOffset(), context.getLimit());
+        } else if (context.getContext() == RULES_COUNT_BY_FILTER_CONTEXT) {
+          return RuleCachedStorage.super.countRulesByFilter(context.getRuleFilter());
         } else {
           throw new IllegalStateException("Unknown context id " + context);
         }
       }
-    };
-    this.ruleFutureCache = new FutureExoCache<>(ruleLoader, ruleCache);
+    }, this.ruleCache);
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public List<Long> findRuleIdsByFilter(RuleFilter ruleFilter, int offset, int limit) {
+    CacheKey key = new CacheKey(RULES_BY_FILTER_CONTEXT, ruleFilter, offset, limit);
+    return (List<Long>) this.ruleFutureCache.get(key, key);
+  }
+
+  @Override
+  public int countRulesByFilter(RuleFilter ruleFilter) {
+    CacheKey key = new CacheKey(RULES_COUNT_BY_FILTER_CONTEXT, ruleFilter);
+    return (int) this.ruleFutureCache.get(key, key);
   }
 
   @Override
   public RuleDTO saveRule(RuleDTO ruleDTO) {
-    try {
-      return super.saveRule(ruleDTO);
-    } finally {
-      if (EntityType.MANUAL.equals(ruleDTO.getType())) {
-        this.ruleFutureCache.clear();
-      } else {
-        clearCache(ruleDTO);
-      }
+    RuleDTO oldRule = ruleDTO.getId() == null ? null : findRuleById(ruleDTO.getId());
+    RuleDTO rule = super.saveRule(ruleDTO);
+    if (oldRule != null) {
+      clearCache(oldRule);
     }
+    clearCache(rule);
+    return rule;
   }
 
   @Override
@@ -115,7 +138,35 @@ public class RuleCachedStorage extends RuleStorage {
 
   @Override
   public void clearCache(RuleDTO rule) {
-    this.ruleFutureCache.remove(new CacheKey(RULE_ID_CONTEXT, rule.getId()).hashCode());
-    this.ruleFutureCache.remove(new CacheKey(RULE_TITLE_CONTEXT, rule.getTitle()).hashCode());
+    if (rule != null) {
+      this.ruleFutureCache.remove(new CacheKey(RULE_ID_CONTEXT, rule.getId()).hashCode());
+      this.ruleFutureCache.remove(new CacheKey(RULE_TITLE_CONTEXT, rule.getTitle()).hashCode());
+    }
+    clearListCache();
   }
+
+  @Override
+  public void clearListCache() {
+    try {
+      this.ruleCache.select(new CachedObjectSelector<>() {
+        @Override
+        public boolean select(Serializable key, ObjectCacheInfo<? extends Object> ocinfo) {
+          return key instanceof CacheKey cacheKey
+                 && (cacheKey.getContext() == RULES_BY_FILTER_CONTEXT
+                     || cacheKey.getContext() == RULES_COUNT_BY_FILTER_CONTEXT);
+        }
+
+        @Override
+        public void onSelect(ExoCache<? extends Serializable, ? extends Object> cache,
+                             Serializable key,
+                             ObjectCacheInfo<? extends Object> ocinfo) throws Exception {
+          cache.remove(key);
+        }
+      });
+    } catch (Exception e) {
+      LOG.warn("Error while fine cache clearing, clear all gamification rule cache entries", e);
+      this.ruleCache.clearCache();
+    }
+  }
+
 }
