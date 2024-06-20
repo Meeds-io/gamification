f37cdd4d4695642091ce4a7bf593c4b3c56a3483
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-06-20 12:49:01 +0100

    feat: Redefine Parent applications Classes to fit layout style properties - MEED-7094 - Meeds-io/MIPs#144 (#1626)
    This change will apply **application-body** class to the main body of
    applications to make sure to apply layout specific CSS styles, such as
    disabling default Vuetify White Background applied on v-card. At the
    same time, for Top Toolbar applications, this will disable branding
    styling to avoid having border radius and other styles applied on small
    buttons added in Top bar as applications.

--- portlets/src/main/webapp/vue-app/connector-admin-settings/components/AdminConnectorSettings.vue
+++ portlets/src/main/webapp/vue-app/connector-admin-settings/components/AdminConnectorSettings.vue
@@ -16,7 +16,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
   <v-app>
-    <v-card class="card-border-radius overflow-hidden" flat>
+    <v-card class="application-body" flat>
       <template v-if="selectedConnector">
         <extension-registry-component
           v-if="editSettings"
