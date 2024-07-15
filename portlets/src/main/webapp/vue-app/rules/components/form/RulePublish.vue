bc74f4e0b4e52789df0d852f1c1a28c6438a3694
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-07-15 12:28:14 +0200

    feat: Update UX of Publish an action activity - MEED-7231 - Meeds-io/MIPs#140 (#1670)
    This PR will update the UX of publish an action activity

--- portlets/src/main/webapp/vue-app/rules/components/form/RulePublish.vue
+++ portlets/src/main/webapp/vue-app/rules/components/form/RulePublish.vue
@@ -2,21 +2,34 @@
   <div
     id="engagementCenterRulePublication"
     class="d-flex flex-column">
-    <div class="d-flex align-center pt-4">
+    <div v-if="published" class="d-flex align-center pt-4">
       <v-card
-        :max-width="spaceId && '50%' || 'calc(100% - 50px)'"
         class="flex-grow-1 flex-shrink-0 text-wrap d-flex text-start me-2"
         flat>
-        {{ $t('rule.form.publishLabel') }}
-        <template v-if="!spaceId">
-          {{ $t('rule.form.personalStream') }}
-        </template>
+        {{ $t('rule.form.contributorsInformed') }}
       </v-card>
-      <div v-if="spaceId" class="flex-grow-1 flex-shrink-1 text-truncate">
-        <exo-space-avatar
-          :space-id="spaceId"
-          :space="space" />
+      <div class="flex-shrink-0 ms-2">
+        <v-btn
+          :href="activityLink"
+          target="_blank"
+          icon
+          small>
+          <v-icon size="20">fas fa-external-link-alt</v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          small
+          @click="publish = true">
+          <v-icon size="20">fas fa-edit</v-icon>
+        </v-btn>
       </div>
+    </div>
+    <div v-else class="d-flex align-center pt-4">
+      <v-card
+        class="flex-grow-1 flex-shrink-0 text-wrap d-flex text-start me-2"
+        flat>
+        {{ $t('rule.form.informContributors') }}
+      </v-card>
       <div class="flex-shrink-0 ms-2">
         <v-switch
           id="engagementCenterRulePublishSwitch"
@@ -24,26 +37,44 @@
           class="my-0 ms-0 me-n1" />
       </div>
     </div>
-    <div v-if="rule.id && !rule.published" class="text-subtitle">
-      {{ $t('rule.form.actionHidden') }}
-    </div>
-    <rich-editor
-      v-if="publish"
-      ref="rulePublicationEditor"
-      id="engagementCenterRulePublishMessage"
-      v-model="message"
-      :max-length="MAX_LENGTH"
-      :template-params="templateParams"
-      :placeholder="$t('rule.form.rulePublicationPlaceholder')"
-      :object-id="metadataObjectId"
-      :suggester-space-id="spaceId"
-      object-type="rule"
-      ck-editor-type="activityContent_rule"
-      class="flex my-3"
-      autofocus
-      tag-enabled
-      @validity-updated="validMessage = $event"
-      @attachments-edited="$emit('attachments-edited')" />
+    <template v-if="publish">
+      <div class="d-flex align-center pt-4">
+        <v-card
+          :max-width="spaceId && '50%' || 'calc(100% - 50px)'"
+          class="text-wrap d-flex text-start me-2"
+          flat>
+          {{ $t('rule.form.publishLabel') }}
+          <template v-if="!spaceId">
+            {{ $t('rule.form.personalStream') }}
+          </template>
+        </v-card>
+        <div v-if="spaceId" class="text-truncate">
+          <exo-space-avatar
+            :space-id="spaceId"
+            :space="space"
+            :size="21" />
+        </div>
+      </div>
+      <div v-if="rule.id && !rule.published" class="text-subtitle">
+        {{ $t('rule.form.actionHidden') }}
+      </div>
+      <rich-editor
+        ref="rulePublicationEditor"
+        id="engagementCenterRulePublishMessage"
+        v-model="message"
+        :max-length="MAX_LENGTH"
+        :template-params="templateParams"
+        :placeholder="$t('rule.form.rulePublicationPlaceholder')"
+        :object-id="metadataObjectId"
+        :suggester-space-id="spaceId"
+        object-type="rule"
+        ck-editor-type="activityContent_rule"
+        class="flex my-3"
+        autofocus
+        tag-enabled
+        @validity-updated="validMessage = $event"
+        @attachments-edited="$emit('attachments-edited')" />
+    </template>
   </div>
 </template>
 <script>
@@ -85,6 +116,15 @@ export default {
     spaceId() {
       return this.program?.spaceId || this.program?.space?.id;
     },
+    published() {
+      return this.rule?.published;
+    },
+    activityId() {
+      return this.rule?.activityId;
+    },
+    activityLink() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/activity?id=${this.activityId}`;
+    }
   },
   watch: {
     publish() {
