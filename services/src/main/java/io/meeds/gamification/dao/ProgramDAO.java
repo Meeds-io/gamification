eff62761ef9e08d9b1727722b25d9dca71121e0f
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-19 06:20:36 +0100

    feat: Adapt Space Service to delete Apps Management - MEED-7638 - Meeds-io/MIPs#150

--- services/src/main/java/io/meeds/gamification/dao/ProgramDAO.java
+++ services/src/main/java/io/meeds/gamification/dao/ProgramDAO.java
@@ -18,7 +18,6 @@
 package io.meeds.gamification.dao;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
