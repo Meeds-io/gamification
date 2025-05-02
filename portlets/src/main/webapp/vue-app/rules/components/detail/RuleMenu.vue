4c47c8e16ebe74f7f98db7e5fe6a9cefb4a3c74b
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-05-02 16:36:43 +0200

    fix: Allow to duplicate Action - MEED-8916 - Meeds-io/meeds#2833 (#1832)
    This change will allow to duplicate actions

--- portlets/src/main/webapp/vue-app/rules/components/detail/RuleMenu.vue
+++ portlets/src/main/webapp/vue-app/rules/components/detail/RuleMenu.vue
@@ -81,6 +81,20 @@
       <v-list-item
         v-if="canEdit"
         dense
+        @click="duplicateRule">
+        <v-list-item-avatar
+          min-width="15"
+          width="15"
+          class="mt-0 mb-auto">
+          <v-icon size="13" class="icon-default-color">fas fa-copy</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-title class="text-start">
+          {{ $t('programs.details.rule.button.duplicate') }}
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item
+        v-if="canEdit"
+        dense
         @click.prevent.stop="enableDisableRule">
         <v-list-item-avatar
           min-width="15"
@@ -203,6 +217,11 @@ export default {
       }
       this.$ruleService.getRuleById(this.rule?.id)
         .then(rule => this.$root.$emit('rule-form-drawer', rule));
+    },
+    duplicateRule() {
+      delete this.rule.id;
+      this.rule.published = false;
+      this.$root.$emit('rule-form-drawer', this.rule);
     },
     copyLink(event) {
       if (event) {
