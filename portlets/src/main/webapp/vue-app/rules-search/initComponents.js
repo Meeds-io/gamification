baed56abb4a5b699c4acd394e56a897dceafba4b
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-05-13 16:57:32 +0100

    feat: Review actions search result display - MEED-8986 - Meeds-io/MIPs#193 (#1852)

--- portlets/src/main/webapp/vue-app/rules-search/initComponents.js
+++ portlets/src/main/webapp/vue-app/rules-search/initComponents.js
@@ -1,7 +1,9 @@
 import RuleSearchCard from './components/RuleSearchCard.vue';
+import RuleDetailDrawer from '../rules/components/drawers/RuleDetailDrawer.vue';
 
 const components = {
   'engagement-center-rule-search-card': RuleSearchCard,
+  'engagement-center-rule-detail-drawer': RuleDetailDrawer,
 };
 
 for (const key in components) {
