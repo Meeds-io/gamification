eb4c46a22df75fa9557745191ed1b9914f1c8d14
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-05-28 15:51:34 +0200

    fix: Fix Infinite loading when charging engagement center resources - MEED-9194 - Meeds-io/MIPs#194 (#1883)
    This change will centralize commons component used for overview page and
    engagement center

--- portlets/src/main/webapp/vue-app/commons/initComponents.js
+++ portlets/src/main/webapp/vue-app/commons/initComponents.js
@@ -27,6 +27,35 @@ import ProgramImageSelector from './components/program/ProgramImageSelector.vue'
 import ProgramImageSelectorButtons from './components/program/ProgramImageSelectorButtons.vue';
 import ProgramOwnerAssignment from './components/program/ProgramOwnerAssignment.vue';
 
+import Programs from './components/program/Programs.vue';
+import ProgramsList from './components/program/ProgramsList.vue';
+import ProgramCard from './components/program/card/ProgramCard.vue';
+import ProgramMenu from './components/program/card/ProgramMenu.vue';
+import ProgramDetail from './components/program/detail/ProgramDetail.vue';
+import ProgramCreatedPlaceholder from './components/program/detail/ProgramCreatedPlaceholder.vue';
+import ProgramDisabledMaskContent from './components/program/card/ProgramDisabledMaskContent.vue';
+import RulesToolbar from './components/program/rules/RulesToolbar.vue';
+import RuleTitle from './components/program/rules/RuleTitle.vue';
+import NoRuleFound from './components/program/rules/NoRuleFound.vue';
+
+import Rules from './components/rule/Rules.vue';
+import RuleCategory from './components/rule/category/Category.vue';
+import RulesList from './components/rule/layout/RulesList.vue';
+import RulesByProgram from './components/rule/layout/RulesByProgram.vue';
+import RulesByTrend from './components/rule/layout/RulesByTrend.vue';
+import RulesFilterDrawer from './components/rule/filter/RulesFilterDrawer.vue';
+import RuleCard from './components/rule/card/RuleCard.vue';
+import RuleCardMaskContent from './components/rule/card/RuleCardMaskContent.vue';
+import RuleCardMaskConnector from './components/rule/card/RuleCardMaskConnector.vue';
+import RuleCardMaskPrequisiteRules from './components/rule/card/RuleCardMaskPrequisiteRules.vue';
+import RuleCardMaskRemainingDates from './components/rule/card/RuleCardMaskRemainingDates.vue';
+import RuleCardMaskRecurrence from './components/rule/card/RuleCardMaskRecurrence.vue';
+import RuleCardMaskAudience from './components/rule/card/RuleCardMaskAudience.vue';
+import RuleCardMaskWhitelist from './components/rule/card/RuleCardMaskWhitelist.vue';
+import RuleCardRecurrence from './components/rule/card/RuleCardRecurrence.vue';
+import RuleCardPoints from './components/rule/card/RuleCardPoints.vue';
+import RuleCardRemainingDates from './components/rule/card/RuleCardRemainingDates.vue';
+
 const components = {
   'gamification-overview-widget': Widget,
   'gamification-overview-widget-row': WidgetRow,
@@ -42,6 +71,35 @@ const components = {
   'gamification-program-owner-assignment': ProgramOwnerAssignment,
 
   'gamification-rule-item': RuleItem,
+  'gamification-rules': Rules,
+  'gamification-rules-category': RuleCategory,
+  'gamification-rules-list': RulesList,
+  'gamification-rules-by-program': RulesByProgram,
+  'gamification-rules-by-trend': RulesByTrend,
+  'gamification-rules-filter-drawer': RulesFilterDrawer,
+  'gamification-rule-card': RuleCard,
+  'gamification-rule-card-mask-content': RuleCardMaskContent,
+  'gamification-rule-card-mask-connector': RuleCardMaskConnector,
+  'gamification-rule-card-mask-prequisite-rules': RuleCardMaskPrequisiteRules,
+  'gamification-rule-card-mask-remaining-dates': RuleCardMaskRemainingDates,
+  'gamification-rule-card-mask-recurrence': RuleCardMaskRecurrence,
+  'gamification-rule-card-mask-audience': RuleCardMaskAudience,
+  'gamification-rule-card-mask-whitelist': RuleCardMaskWhitelist,
+  'gamification-rule-card-recurrence': RuleCardRecurrence,
+  'gamification-rule-card-points': RuleCardPoints,
+  'gamification-rule-card-remaining-dates': RuleCardRemainingDates,
+
+  'gamification-programs': Programs,
+  'gamification-programs-list': ProgramsList,
+  'gamification-program-card': ProgramCard,
+  'gamification-program-menu': ProgramMenu,
+  'gamification-program-detail': ProgramDetail,
+  'gamification-program-created-placeholder': ProgramCreatedPlaceholder,
+  'gamification-program-disabled-mask-content': ProgramDisabledMaskContent,
+  'gamification-program-rules-toolbar': RulesToolbar,
+  'gamification-program-rule-item': RuleItem,
+  'gamification-program-rule-title': RuleTitle,
+  'gamification-program-no-rule-found': NoRuleFound,
 };
 
 for (const key in components) {
