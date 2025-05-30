583536bee4d4d5310af27b244e6acde8473358e3
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-05-30 11:42:39 +0100

    feat: Remove bold text from rule search result title - MEED-9212 - Meeds-io/MIPs#193 (#1888)

--- portlets/src/main/webapp/vue-app/rules-search/initComponents.js
+++ portlets/src/main/webapp/vue-app/rules-search/initComponents.js
@@ -1,9 +1,7 @@
 import RuleSearchCard from './components/RuleSearchCard.vue';
-import RuleDetailDrawer from '../rules/components/drawers/RuleDetailDrawer.vue';
 
 const components = {
   'engagement-center-rule-search-card': RuleSearchCard,
-  'engagement-center-rule-detail-drawer': RuleDetailDrawer,
 };
 
 for (const key in components) {
